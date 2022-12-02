ecd5743712c3be16d46a7f3e52f84ffa593c3001
ulisesGutierrez <techwithulises@gmail.com>
2022-12-02 13:47:06 -0600

    scope

---
+++ variables.js
@@ -0,0 +1,22 @@
+//Tipos de datos:
+string = "cadena de texto"
+numero = 19
+booleano = true
+
+//Existe en el programa, pero no tiene valor:
+let undefined
+alert(undefined)
+
+//declaracion de variables: var, let, const:
+let numerodos;
+
+//Inicializarla dandole valor:
+numerodos = 30
+
+//Declarada e inicializada (const debe ser declarada e inicializada, si no tira error):
+let numero = 40
+//Dandole otro valor:
+numero = 50
+/* 
+Scope: Alcance de las variable, es mala práctica usar var, es mejor let, tiene menos alcance.
+*/
