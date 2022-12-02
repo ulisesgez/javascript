cca518afefb2e43c376bfd58458c3d8238aea0a7
ulisesGutierrez <techwithulises@gmail.com>
2022-12-02 14:32:58 -0600

    prompt

---
+++ javascript/nullNan.js
@@ -0,0 +1,9 @@
+// Nos dice que la variable esta vacia, pero es intencional, a diferencia de undefined, con null le decimos que no va a tener un valor (nulo o vacio):
+let numero = null;
+
+//Nan o no es un numero:
+
+let numero2 = 5;
+let numero3 = "pedro";
+
+alert(numero2 * numero3);
---
+++ javascript/promp.js
@@ -0,0 +1,7 @@
+//Prompt es una funcion, la cual nos va devolver un valor de lo que nosotros le escribamos.
+prompt("helloWorld");
+/**
+ * Como guardamos el valor introducido? Con las variables
+ */
+let nombre = prompt("What is your name?");
+alert("helo " + nombre);
