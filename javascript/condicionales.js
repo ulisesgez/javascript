028bbdf7f639b2c6852a07a8331aea8a9d0dc7be
ulisesGutierrez <techwithulises@gmail.com>
2022-12-07 21:26:24 -0600

    condicionales

---
+++ javascript/condicionales.js
@@ -0,0 +1,12 @@
+nombre = "alex";
+texto = "tu nombre es ";
+
+if (nombre == "alex") {
+    alert(texto + nombre);
+}
+else if (nombre == "jhon") {
+    alert(texto + nombre);
+}
+else {
+    alert("no se quien eres");
+}
