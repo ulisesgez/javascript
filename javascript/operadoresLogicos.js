60e2de95e9ee0524337027d0ed315c98cb8bef9e
ulisesGutierrez <techwithulises@gmail.com>
2022-12-07 19:53:35 -0600

    operadoresLogicos

--- javascript/operadoresLogicos.js
+++ javascript/operadoresLogicos.js
@@ -3,3 +3,14 @@
  * una condicióiin, su resultado es booleano y 
  * sus operandos son valores logicos o asimilables a ellos
  */
+
+let valor = true;
+let valor2 = true;
+//true, ya que si las dos condiciones se cumplen devuelve true
+let resultado = valor && valor2;
+//true, ya que si con que una sea verdadera devuelve verdadero
+let resultado2 = valor || valor2;
+//false, negación de true
+let resultado3 = !valor;
+
+document.write(resultado)
