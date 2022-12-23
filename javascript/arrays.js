3991dc568af6c05f50a9d29bb7727034c9ee8bd3
ulisesGutierrez <techwithulises@gmail.com>
2022-12-22 20:41:07 -0600

    createdarrays

---
+++ javascript/arrays.js
@@ -0,0 +1,11 @@
+/**
+ * Array: Conjunto de datos, alos que despues podemos llamar, estos estan asociados a un indice, el primer elemento se encuentra en la posicion 0
+ */
+//Ejemplo:
+let numeros = ["uno", 2, "tres", 4];
+
+//Mostrar por posisiciones, en este caso se imprimira "uno":
+document.write(numeros[0]);
+
+//Tira undefined, por que no esta definido - declarada:
+document.write(numeros[4]);
