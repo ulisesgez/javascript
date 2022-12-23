2f63a1bb5d1ffb2a616d975bc44d477103849681
ulisesGutierrez <techwithulises@gmail.com>
2022-12-22 21:23:33 -0600

    arraysasociativos

--- javascript/arrays.js
+++ javascript/arrays.js
@@ -9,3 +9,24 @@ document.write(numeros[0]);
 
 //Tira undefined, por que no esta definido - declarada:
 document.write(numeros[4]);
+
+/**
+ * Arrays Asociativos: Parecidos al formato json
+ */
+//Ejemplo:
+let pc = {
+    nombre: "mypc",
+    procesador: "Intel",
+    ram: "16GB",
+    espacio: "1TB"
+};
+
+//Para este caso si queremos mandar a imprimir o tomar un elemento lo hacemos de la siguiente forma:
+document.write(pc["nombre"]);
+
+frase = `Nombre de pc: ${nombre}, <br>
+        procesador: ${procesador}, <br>
+        memoria ram: ${ram}, <br>
+        almacenamiento: ${espacio}`
+
+document.write(frase);
