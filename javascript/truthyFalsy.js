a8d5bf9e3c2f830b8a8995ee3d9a01a7ee8f6c7d
ulisesGutierrez <techwithulises@gmail.com>
2022-12-28 14:43:23 -0600

    truthyFalsy

---
+++ javascript/truthyFalsy.js
@@ -0,0 +1,13 @@
+//Tipo de valor verdadero y falso:
+
+Boolean(0);//falso
+Boolean(null);
+Boolean(NaN);
+Boolean(undefined);
+Boolean(false);
+Boolean("");
+
+Boolean(1);//Verdadero
+Boolean("a");
+Boolean([]);
+Boolean({});
