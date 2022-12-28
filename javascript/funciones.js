256e6166156a0c427fc66f7087bd0835eec2fd2c
ulisesGutierrez <techwithulises@gmail.com>
2022-12-28 08:15:09 -0600

    functions

---
+++ javascript/funciones.js
@@ -0,0 +1,18 @@
+/**
+ * Funciones: 
+ */
+
+function saludar(){
+    respuesta = prompt("hello, how is your day today?");
+    if (respuesta == "bien"){
+        alert("cool")
+    } else {
+        alert("sorry bro")
+    }
+}
+
+//Se repetira 3 veces:
+
+saludar();
+saludar();
+saludar();
