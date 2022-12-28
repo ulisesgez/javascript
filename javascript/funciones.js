8742545694f3146c879cb9481f623e42babc7e38
ulisesGutierrez <techwithulises@gmail.com>
2022-12-28 08:28:17 -0600

    return

--- javascript/funciones.js
+++ javascript/funciones.js
@@ -16,3 +16,26 @@ function saludar(){
 saludar();
 saludar();
 saludar();
+
+//Asigar funcion en variable:
+
+saludar = function(){
+    respuesta = prompt("hello, how is your day today?");
+    if (respuesta == "bien"){
+        alert("cool")
+    } else {
+        alert("sorry bro")
+    }
+}
+
+//Return en funciones, sin la linea return "exit", marca undefined:
+//saludo es igual a lo que retorna.
+
+function saludardos(){
+    alert("hola");
+    return "exit"
+    alert("no retorna");
+}
+
+let saludo = saludardos();
+document.write(saludo);
