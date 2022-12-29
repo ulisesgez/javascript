5cf729a1c7e08a93884862c85deb348941be845c
ulisesGutierrez <techwithulises@gmail.com>
2022-12-29 14:24:20 -0600

    switch

---
+++ javascript/switch.js
@@ -0,0 +1,14 @@
+let numero = 1;
+switch (numero){
+    case 1:
+        console.log("soy uno");
+        break;
+    case 10:
+        console.log("soy diez");
+        break;
+    case 100:
+        console.log("Soy un 100");
+        break;
+    default:
+        console.log("No es un numero");
+}
