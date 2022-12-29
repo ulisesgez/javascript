236ad028e71b4741766522dc17505c6d96108ba5
ulisesGutierrez <techwithulises@gmail.com>
2022-12-29 17:04:27 -0600

    forOf

--- javascript/loopsCiclos.js
+++ javascript/loopsCiclos.js
@@ -1,3 +1,5 @@
+//Ejemplo con for:
+
 let estudiantes = ["Maria", "Sergio", "Rosa", "Daniel"];
 
 function saludarEstudiantes(estudiante){
@@ -7,3 +9,26 @@ function saludarEstudiantes(estudiante){
 for (let i = 0; i < estudiantes.length; i++){
     saludarEstudiantes(estudiantes[i]);
 }
+/**
+ *  Hola, Maria
+ *  Hola, Sergio
+ *  Hola, Rosa
+ *  Hola, Daniel
+ */
+
+//Otra forma de crear el saludo:
+
+function saludarEstudiantes(estudiante){
+    console.log(`Hola, ${estudiante}`);
+}
+
+for (let estudiante of estudiantes){
+    saludarEstudiantes(estudiante);
+}
+
+/**
+ *  Hola, Maria
+ *  Hola, Sergio
+ *  Hola, Rosa
+ *  Hola, Daniel
+ */
