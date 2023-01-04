d767ad835e2f0de77c5353dc589a6cc742875369
ulisesGutierrez <techwithulises@gmail.com>
2023-01-04 16:00:22 -0600

    concatenacionMasIgual

--- javascript/concatenacion.js
+++ javascript/concatenacion.js
@@ -73,3 +73,19 @@ frase = "mi nombre es 'Ulises'y soy el mejor";
 frase = 'mi nombre es "Ulises"y soy el mejor';
 //o
 frase = "mi nombre es 'Ulises'y soy el mejor";
+
+//Concatenación de cadenas con el operador más igual a
+
+/*
+También podemos usar el +=operador para concatenar una cadena al final de una
+variable de cadena existente. Esto puede ser muy útil para dividir una cadena
+larga en varias líneas.
+
+Nota: Cuidado con los espacios. La concatenación no agrega espacios entre cadenas
+concatenadas, por lo que deberá agregarlos usted mismo.
+*/
+
+let ourStr = "I come first. ";
+ourStr += "I come second.";
+
+// ourStrahora tiene un valor de la cadena I come first. I come second..
