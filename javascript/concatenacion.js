d6581f8dc91a2b6ba1f05e79d83575fdb069c22d
ulisesGutierrez <techwithulises@gmail.com>
2023-01-06 18:04:23 -0600

    notacionDeCorchetes

--- javascript/concatenacion.js
+++ javascript/concatenacion.js
@@ -114,3 +114,23 @@ let freeStr = "freeCodeCamp is ";
 ourStr += anAdjective;
 
 //ourStrtendría el valor freeCodeCamp is awesome!.
+
+//Use la notación de corchetes para encontrar el primer carácter en una cadena
+
+/*
+La notación de corchetes es una forma de obtener un carácter en un índice
+específico dentro de una cadena.
+
+La mayoría de los lenguajes de programación modernos, como JavaScript,
+no comienzan a contar desde 1 como lo hacen los humanos. Comienzan en 0.
+Esto se conoce como indexación basada en cero .
+
+Por ejemplo, el carácter en el índice 0 de la palabra Charleses C.
+Entonces const firstName = "Charles", si puede obtener el valor de la primera
+letra de la cadena usando firstName[0].
+*/
+
+const firstName = "Charles";
+const firstLetter = firstName[0];
+
+//firstLettertendría un valor de la cadena C.
