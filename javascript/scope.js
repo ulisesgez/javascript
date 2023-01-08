52bd9f9a2a32e8eef7b07e505809c69da422a08b
ulisesGutierrez <techwithulises@gmail.com>
2023-01-08 09:47:51 -0600

    alcance global

--- javascript/scope.js
+++ javascript/scope.js
@@ -14,3 +14,15 @@ function nombre(){
 nombre();//Imprime Ulises Gutierrez
 miNombre//Esto en consola imprime Ulises
 miApellido//Esto en consola no imprime Gutierrez
+
+//Alcance global y funciones
+
+/*
+En JavaScript, el alcance se refiere a la visibilidad de las variables.
+Las variables que se definen fuera de un bloque de funciones tienen alcance global .
+Esto significa que se pueden ver en todas partes en su código JavaScript.
+
+Las variables que se declaran sin las palabras clave leto constse crean automáticamente
+en el globalalcance. Esto puede crear consecuencias no deseadas en otras partes de su código
+o al ejecutar una función nuevamente. Siempre debe declarar sus variables con leto const.
+*/
