b8cf61438858bb03d3185502396987cf9aabf321
ulisesGutierrez <techwithulises@gmail.com>
2023-01-08 09:34:42 -0600

    parametrosArgumentos

--- javascript/funciones.js
+++ javascript/funciones.js
@@ -82,6 +82,26 @@ function suma(num1, num2){
 suma(1, 2);
 suma(5, 10);
 
+/*
+Los parámetros son variables que actúan como marcadores de posición para los valores que 
+se deben ingresar a una función cuando se la llama. Cuando se define una función,
+normalmente se define junto con uno o más parámetros. Los valores reales que se ingresan (o "pasan" )
+en una función cuando se llama se conocen como argumentos .
+
+Aquí hay una función con dos parámetros, param1y param2:
+*/
+
+function testFun(param1, param2) {
+    console.log(param1, param2);
+}
+
+/*
+Entonces podemos llamar testFunasí: testFun("Hello", "World");.
+Hemos pasado dos argumentos de cadena, Helloy World. Dentro de la función, param1será igual 
+a la cadena Helloy param2será igual a la cadena World. Tenga en cuenta que podría testFunvolver
+ a llamar con diferentes argumentos y los parámetros tomarían el valor de los nuevos argumentos.
+*/
+
 //Usando return:
 
 function resta(num3, num4){
