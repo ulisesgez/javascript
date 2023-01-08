1532db1bdfb5e2567bd5522e2fa9b7d8fbc02cb4
ulisesGutierrez <techwithulises@gmail.com>
2023-01-07 19:38:45 -0600

    accederArreglos

--- javascript/arrays.js
+++ javascript/arrays.js
@@ -49,3 +49,33 @@ frase = `Nombre de pc: ${nombre}, <br>
         almacenamiento: ${espacio}`
 
 document.write(frase);
+
+//Acceda a arreglos multidimensionales con índices
+
+/*
+Una forma de pensar en una matriz multidimensional es como una matriz de
+matrices . Cuando usa corchetes para acceder a su matriz, el primer conjunto
+de corchetes se refiere a las entradas en la matriz más externa (el primer nivel),
+y cada par adicional de corchetes se refiere al siguiente nivel de entradas 
+en el interior.
+*/
+
+const arr = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+    [[10, 11, 12], 13, 14]
+  ];
+  
+const subarray = arr[3];
+const nestedSubarray = arr[3][0];
+const element = arr[3][0][1];
+  
+/*
+En este ejemplo, subarraytiene el valor [[10, 11, 12], 13, 14], 
+nestedSubarraytiene el valor [10, 11, 12]y elementtiene el valor 11.
+Nota: No debe haber espacios entre el nombre de la matriz y los corchetes,
+como array [0][0]e incluso esto array [0] [0]no está permitido.
+Aunque JavaScript puede procesar esto correctamente, esto puede confundir a 
+otros programadores al leer su código.
+*/
