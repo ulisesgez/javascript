a06c2e15ab13b5558195a07a1f92055da1e6faeb
ulisesGutierrez <techwithulises@gmail.com>
2023-01-07 19:56:43 -0600

    pop

--- javascript/arraysRecorridos.js
+++ javascript/arraysRecorridos.js
@@ -135,7 +135,24 @@ let shiftArray = array.shift()
 //Revisamos. El output debe de ser [2,3,4,5]
 console.log(array)
 
-// --- POP ---
+// Manipular arreglos con pop()
+
+/*
+Otra forma de cambiar los datos en una matriz es con la .pop()función.
+.pop()se utiliza para extraer un valor del final de una matriz.
+Podemos almacenar este valor extraído asignándolo a una variable.
+En otras palabras, .pop()elimina el último elemento de una matriz y devuelve ese elemento.
+Cualquier tipo de entrada se puede extraer de una matriz: números, cadenas, incluso matrices anidadas.
+*/
+
+const threeArr = [1, 4, 6];
+const oneDown = threeArr.pop();
+console.log(oneDown);
+console.log(threeArr);
+
+// El primero console.logmostrará el valor 6y el segundo mostrará el valor [1, 4].
+
+//Otro ejemplo:
 
 //Creamos el array
 let arrayDos = [1,2,3,4,5]
