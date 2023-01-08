9ebaeadcb87b675f55992dd167ed1124e9ef39de
ulisesGutierrez <techwithulises@gmail.com>
2023-01-07 19:53:06 -0600

    push

--- javascript/arraysRecorridos.js
+++ javascript/arraysRecorridos.js
@@ -84,6 +84,27 @@ let cheapTelevisions = televisions.some(function(television){
 
 console.log(cheapTelevisions);//true
 
+
+//Manipular arreglos con push()
+
+/*
+Una manera fácil de agregar datos al final de una matriz es a través de la push()función.
+
+.push()toma uno o más parámetros y los "empuja" al final de la matriz.
+
+Ejemplos:
+*/
+
+const arr1 = [1, 2, 3];
+arr1.push(4);
+
+const arr2 = ["Stimpson", "J", "cat"];
+arr2.push(["happy", "joy"]);
+
+/*
+arr1ahora tiene el valor [1, 2, 3, 4]y arr2tiene el valor ["Stimpson", "J", "cat", ["happy", "joy"]].
+*/
+
 /**
  * El método .push() nos permite agregar uno o más elementos al final 
  * de un array. A continuación veremos un ejemplo aplicado con un array 
