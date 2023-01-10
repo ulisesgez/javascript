71d0c970b41465b02e9f9e1fa799fd6d6360a863
ulisesGutierrez <techwithulises@gmail.com>
2023-01-10 14:37:49 -0600

    trueOrFalse

--- javascript/truthyFalsy.js
+++ javascript/truthyFalsy.js
@@ -11,3 +11,16 @@ Boolean(1);//Verdadero
 Boolean("a");
 Boolean([]);
 Boolean({});
+
+//Comprender los valores booleanos
+
+/*
+Otro tipo de datos es el booleano .
+Los valores booleanos solo pueden ser uno de dos valores: trueo false.
+Son básicamente pequeños interruptores de encendido y apagado, donde trueestá encendido y
+falseapagado. Estos dos estados son mutuamente excluyentes.
+
+Nota: los valores booleanos nunca se escriben entre comillas.
+Las cadenas "true"y "false"no son booleanas y no tienen un
+significado especial en JavaScript
+*/
