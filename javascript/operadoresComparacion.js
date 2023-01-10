659b99239e713a35cfca6af4f6a1086eb1033e75
ulisesGutierrez <techwithulises@gmail.com>
2023-01-10 15:55:51 -0600

    comparacionOperadores

--- javascript/operadoresComparacion.js
+++ javascript/operadoresComparacion.js
@@ -1,3 +1,37 @@
+//Comparación con el Operador de Igualdad
+
+/*
+Hay muchos operadores de comparación en JavaScript. Todos estos operadores devuelven un
+valor booleano trueo false.
+
+El operador más básico es el operador de igualdad ==. El operador de igualdad compara
+dos valores y devuelve truesi son equivalentes o falseno. Tenga en cuenta que la igualdad
+es diferente de la asignación ( =), que asigna el valor a la derecha del operador a una
+variable a la izquierda.
+*/
+
+function equalityTest(myVal) {
+    if (myVal == 10) {
+        return "Equal";
+    }
+    return "Not Equal";
+}
+
+/*
+Si myVales igual a 10, el operador de igualdad devuelve true, por lo que el código 
+entre llaves se ejecutará y la función devolverá Equal. De lo contrario, la función
+devolverá Not Equal. Para que JavaScript compare dos tipos de datos diferentes 
+(por ejemplo, numbersy strings), debe convertir un tipo en otro.
+Esto se conoce como tipo de coerción. Sin embargo, una vez que lo hace, puede comparar 
+términos de la siguiente manera:
+
+1   ==  1  // true
+1   ==  2  // false
+1   == '1' // true
+"3" ==  3  // true
+
+*/
+
 /**
  * Operadores de compración: Comparan dos expresiones y
  * devuelven un valor Boolean que representa la relación de sus valores
