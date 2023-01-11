76b0d0c9786b42e0b995f4a8d82b249d0edcda77
ulisesGutierrez <techwithulises@gmail.com>
2023-01-10 20:54:02 -0600

    mayorOIgualQue

--- javascript/operadoresComparacion.js
+++ javascript/operadoresComparacion.js
@@ -98,6 +98,24 @@ Ejemplos
 '1' >  9  // false
 */
 
+/*
+Comparación con el operador mayor o igual que
+
+El operador mayor o igual que ( >=) compara los valores de dos números.
+Si el número de la izquierda es mayor o igual que el número de la derecha,
+devuelve true. De lo contrario, vuelve false.
+
+Al igual que el operador de igualdad, el operador mayor o igual que
+convertirá los tipos de datos durante la comparación.
+
+Ejemplos
+
+6   >=  6  // true
+7   >= '3' // true
+2   >=  3  // false
+'7' >=  9  // false
+*/
+
 /**
  * Operadores de compración: Comparan dos expresiones y
  * devuelven un valor Boolean que representa la relación de sus valores
