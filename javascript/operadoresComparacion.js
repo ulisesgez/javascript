83ec7b9df293f4e7f3834bae4e77478f970c7df9
ulisesGutierrez <techwithulises@gmail.com>
2023-01-10 20:47:10 -0600

    operadorDesigualdad

--- javascript/operadoresComparacion.js
+++ javascript/operadoresComparacion.js
@@ -49,6 +49,22 @@ Ejemplos:
 3 === '3' // false
 */
 
+/*
+Comparación con el Operador de Desigualdad
+El operador de desigualdad ( !=) es lo opuesto al operador de igualdad.
+Significa no igual y vuelve falsedonde volvería la igualdad truey 
+viceversa . Al igual que el operador de igualdad, el operador de 
+desigualdad convertirá tipos de datos de valores al comparar.
+
+Ejemplos
+
+1 !=  2    // true
+1 != "1"   // false
+1 != '1'   // false
+1 != true  // false
+0 != false // false
+*/
+
 /**
  * Operadores de compración: Comparan dos expresiones y
  * devuelven un valor Boolean que representa la relación de sus valores
