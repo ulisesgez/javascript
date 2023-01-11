cfeeb545ad2d90e9893f710a63fe265eb994811c
ulisesGutierrez <techwithulises@gmail.com>
2023-01-10 20:51:35 -0600

    operadorMayorque

--- javascript/operadoresComparacion.js
+++ javascript/operadoresComparacion.js
@@ -67,6 +67,7 @@ Ejemplos
 
 /*
 Comparación con el Operador de Desigualdad Estricta
+
 El operador de desigualdad estricta ( !==) es el opuesto lógico del 
 operador de igualdad estricta. Significa "Estrictamente no igual" y 
 regresa falsedonde regresaría la igualdad estricta truey viceversa .
@@ -77,6 +78,24 @@ Ejemplos
 3 !==  3  // false
 3 !== '3' // true
 4 !==  3  // true
+*/
+
+/*
+Comparación con el operador mayor que
+
+El operador mayor que ( >) compara los valores de dos números.
+Si el número de la izquierda es mayor que el número de la derecha,
+devuelve true. De lo contrario, vuelve false.
+
+Al igual que el operador de igualdad, el operador mayor que convertirá
+tipos de datos de valores al comparar.
+
+Ejemplos
+
+5   >  3  // true
+7   > '3' // true
+2   >  3  // false
+'1' >  9  // false
 */
 
 /**
