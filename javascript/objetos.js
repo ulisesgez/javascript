5ad9316674194b234a8184c6209cff02bb84a313
ulisesGutierrez <techwithulises@gmail.com>
2023-01-13 09:43:33 -0600

    accesToObjects

--- javascript/objetos.js
+++ javascript/objetos.js
@@ -27,12 +27,36 @@ const anotherObject = {
     make: "Ford",
     5: "five",
     "model": "focus"
-  };
+};
 
-  /*
-  Sin embargo, si su objeto tiene propiedades que no son cadenas,
-  JavaScript las encasillará automáticamente como cadenas.
-  */
+/*
+Sin embargo, si su objeto tiene propiedades que no son cadenas,
+JavaScript las encasillará automáticamente como cadenas.
+*/
+
+/*
+Acceso a propiedades de objetos con notación de puntos:
+
+Hay dos formas de acceder a las propiedades de un objeto: notación de puntos ( .) 
+y notación de corchetes ( []), similar a una matriz.
+
+La notación de puntos es lo que usa cuando sabe el nombre de la propiedad a la
+que intenta acceder con anticipación.
+
+Aquí hay una muestra del uso de la notación de puntos ( .) para leer la
+propiedad de un objeto:
+*/
+
+const myObj = {
+    prop1: "val1",
+    prop2: "val2"
+};
+const prop1val = myObj.prop1;
+const prop2val = myObj.prop2;
+
+/*
+prop1valtendría un valor de la cadena val1y prop2valtendría un valor de la cadena val2.
+*/
 
 /*
 Objeto: Algo fisico al paradigma de javascript
