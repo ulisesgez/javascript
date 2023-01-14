bb494dccabd77a235ddeb0585dd67f8dc5ae2cb6
ulisesGutierrez <techwithulises@gmail.com>
2023-01-14 08:42:00 -0600

    anadirPropiedadesObjeto

--- javascript/objetos.js
+++ javascript/objetos.js
@@ -145,6 +145,33 @@ ourDog.name = "Happy Camper";o ourDog["name"] =
 en lugar de obtener Camper, obtendremos su nuevo nombre, Happy Camper.
 */
 
+//Agregar nuevas propiedades a un objeto de JavaScript
+
+/*
+Puede agregar nuevas propiedades a los objetos de JavaScript
+existentes de la misma manera que los modificaría.
+
+Así es como agregaríamos una barkpropiedad a ourDog:
+
+ourDog.bark = "bow-wow";
+
+o
+
+ourDog["bark"] = "bow-wow";
+
+Ahora, cuando evaluemos ourDog.bark, obtendremos su ladrido, bow-wow.
+Ejemplo:
+*/
+
+const yourDog = {
+    "name": "Camper",
+    "legs": 4,
+    "tails": 1,
+    "friends": ["everything!"]
+};
+
+yourDog.bark = "bow-wow";
+
 /*
 Objeto: Algo fisico al paradigma de javascript
 */
