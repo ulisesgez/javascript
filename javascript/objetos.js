df144d06d2e29283348257ef04b9ab56ce66bc66
ulisesGutierrez <techwithulises@gmail.com>
2023-01-14 08:39:13 -0600

    actualizacionPropiedades

--- javascript/objetos.js
+++ javascript/objetos.js
@@ -117,6 +117,34 @@ acceder a la propiedad porque estamos usando el
 valor de la variable, no el nombre .
 */
 
+//Actualización de propiedades de objetos
+
+/*
+Una vez que haya creado un objeto de JavaScript,
+puede actualizar sus propiedades en cualquier momento,
+tal como actualizaría cualquier otra variable.
+Puede usar la notación de puntos o corchetes para actualizar.
+
+Por ejemplo, veamos ourDog:
+*/
+
+const ourDog = {
+    "name": "Camper",
+    "legs": 4,
+    "tails": 1,
+    "friends": ["everything!"]
+};
+
+/*
+Ya que es un perro particularmente feliz,
+cambiemos su nombre a la cadena Happy Camper.
+Así es como actualizamos la propiedad
+de nombre de su objeto:
+ourDog.name = "Happy Camper";o ourDog["name"] =
+"Happy Camper";Ahora, cuando evaluamos ourDog.name,
+en lugar de obtener Camper, obtendremos su nuevo nombre, Happy Camper.
+*/
+
 /*
 Objeto: Algo fisico al paradigma de javascript
 */
