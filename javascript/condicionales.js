c7e64c54a395f3e8ca506bea378cf7b3b0b0730e
ulisesGutierrez <techwithulises@gmail.com>
2023-01-17 14:45:06 -0600

    multiplesTernearios

--- javascript/condicionales.js
+++ javascript/condicionales.js
@@ -111,3 +111,44 @@ function findGreater(a, b) {
 function findGreater(a, b) {
     return a > b ? "a is greater" : "b is greater or equal";
 }
+
+/*
+Usar múltiples operadores condicionales (ternarios)
+
+En el desafío anterior, utilizó un solo operador condicional.
+También puede encadenarlos para verificar múltiples condiciones.
+
+La siguiente función usa declaraciones if,
+else ify elsepara verificar varias condiciones:
+*/
+
+function findGreaterOrEqual(a, b) {
+    if (a === b) {
+      return "a and b are equal";
+    }
+    else if (a > b) {
+      return "a is greater";
+    }
+    else {
+      return "b is greater";
+    }
+}
+
+//La función anterior se puede reescribir usando múltiples operadores condicionales:
+
+function findGreaterOrEqual(a, b) {
+    return (a === b) ? "a and b are equal" 
+      : (a > b) ? "a is greater" 
+      : "b is greater";
+}
+
+/*
+Se considera una buena práctica formatear múltiples operadores condicionales
+de modo que cada condición esté en una línea separada, como se muestra arriba.
+El uso de varios operadores condicionales sin la sangría adecuada puede dificultar
+la lectura del código. Por ejemplo:
+*/
+
+function findGreaterOrEqual(a, b) {
+    return (a === b) ? "a and b are equal" : (a > b) ? "a is greater" : "b is greater";
+}
