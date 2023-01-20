8a72277aa6d48cab69ce827b15252875d0e9f634
ulisesGutierrez <techwithulises@gmail.com>
2023-01-20 13:41:22 -0600

    breakAndContinue

--- javascript/break.js
+++ javascript/break.js
@@ -14,3 +14,16 @@ while(numero <10000){
         break;
     };
 };
+
+
+//Diferencia entre break y continue:
+
+const numeros = [1, ,2, 3, 4, 5, 6, 7, 8, 9, 0];
+
+for (let i = 0; i < numeros.length; i++) {
+    if (i === 5){
+        break;// Imprime esto y no lo demas del 1 al 5, ya que break no saca
+    }
+    console.log(i);//0 al 9
+    console.log(numeros[i]);//1 al 0
+}
--- javascript/continue.js
+++ javascript/continue.js
@@ -6,3 +6,14 @@ for (let i = 0; i < 20; i++){
     }
     document.write(i);
 };
+
+//Diferencia entre break y continue:
+
+const numeros = [1, ,2, 3, 4, 5, 6, 7, 8, 9, 0];
+
+for (let i = 0; i < numeros.length; i++) {
+    if (i === 5){
+        continue;// Saltamos el numero 6,no se imprime, ya que se encuentra en la posicion 5
+    }
+    console.log(numeros[i]);//1 al 0
+}
