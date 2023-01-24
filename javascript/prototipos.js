277077ebab2285ffeada78f0b1898331119a05c7
ulisesGutierrez <techwithulises@gmail.com>
2023-01-23 18:45:53 -0600

    prototipos

---
+++ javascript/prototipos.js
@@ -0,0 +1,41 @@
+//Función constructora:
+
+function Animal(nombre, genero) {
+    //Atributos:
+    this.nombre = nombre;
+    this.genero = genero;
+
+    //Metodos:
+    this.sonar = function() {
+        console.log(`Hago sonido por que estoy vivo`);
+    }
+}
+
+const snoopy = new Animal("snoopy", "macho");
+const lolaBunny = new Animal("lola", "hembra");
+
+console.log(snoopy);
+console.log(lolaBunny);
+
+//Funcion constructora, donde asignamos los metodos al prototipo, no a la función como tal:
+
+function Carro(modelo, año) {
+    //Atributos:
+    this.modelo = modelo;
+    this.año = año;
+
+}
+
+//Metodo agregado al prototipo de la funcion constructora:
+
+Carro.prototype.acelerar = function() {
+    console.log(`run run`);
+}
+
+const tesla = new Carro("Model S", 2022);
+const nissan = new Carro("tsuru", 2017);
+
+console.log(tesla);
+console.log(nissan);
+
+tesla.acelerar();
