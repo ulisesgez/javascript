88cf65668523bf1e7c049dca4fda148b155f56d8
ulisesGutierrez <techwithulises@gmail.com>
2023-01-24 15:55:06 -0600

    getSet

--- javascript/gettersSetters.js
+++ javascript/gettersSetters.js
@@ -39,3 +39,68 @@ const pajaro = new Animal("pajaro", 7, "verde");
 
 Perro.setRaza = "Pedro";//Pedro, buen setter
 document.write(perro.getRaza);//pedro
+
+//Otro ejemplo:
+
+/*
+setters y getters son metodos especiales que permiten establecer y obtener los valores
+de atributos de nuestra clase
+
+Son funciones a traves de las cuales
+podemos leer o modificar un atributo del objeto de manera segura.
+*/
+
+class Carro {
+    constructor(modelo, anio) {
+        //Atributos:
+        this.modelo = modelo;
+        this.anio = anio;
+    }
+    //Metodos:
+    acelerar () {
+        console.log(`run run`);
+    }
+}
+
+class Electrico extends Carro {
+    constructor(modelo, anio, carga) {
+        //super manda a llamar el constructor de la clase padre:
+        super(modelo, anio);
+        this.carga = carga;
+        this.velocidad = null;
+    }
+
+    //Sobreescritura de metodos del prototipo padre en el hijo:
+    acelerar() {
+        console.log("no hago sonidos");
+    }
+    motorUnico() {
+        console.log("No hay combustion interna");
+    }
+    //Ejemplo de metodo estatico:
+    static queSoy() {
+        console.log("Los autos eléctricos son aquellos cuyo motor es capaz de convertir la energía de una batería en energía mecánica");
+    }
+    get getVelocidad() {
+        return this.velocidad;
+    }
+    set setVelocidad(velocidad) {
+        this.velocidad = velocidad;
+    }
+}
+
+//Ejecutando metodo estatico:
+Electrico.queSoy();
+
+const tesla = new Electrico("Model S", 2022, "%80");
+const nissan = new Carro("tsuru", 2017);
+
+console.log(tesla);
+console.log(nissan);
+
+tesla.acelerar();
+nissan.acelerar();
+
+console.log(tesla.getVelocidad);//null
+tesla.setVelocidad = "30km/h";
+console.log(tesla.getVelocidad);//30km/h
--- javascript/metodoEstatico.js
+++ javascript/metodoEstatico.js
@@ -24,10 +24,61 @@ class Perro extends Animal {
     }
 }
 
+//Relacion con seccion de clases
+
 //No podemos llamar a un objeto igual a la clase
+
 let gato = new Animal("gato", 6, "negro");
 let pajaro = new Animal("pajaro", 7, "verde");
 
 Perro.ladrar();//wof, wof
 
-//Relacion con seccion de clases
+//Otro ejemplo, tomando otra referencia:
+
+/*
+Metodo estatico es aquel que se puede ejecutar sin necesidad de instanciar la clase.
+*/
+
+class Carro {
+    constructor(modelo, anio) {
+        //Atributos:
+        this.modelo = modelo;
+        this.anio = anio;
+    }
+    //Metodos:
+    acelerar () {
+        console.log(`run run`);
+    }
+}
+
+class Electrico extends Carro {
+    constructor(modelo, anio, carga) {
+        //super manda a llamar el constructor de la clase padre:
+        super(modelo, anio);
+        this.carga = carga;
+    }
+
+    //Sobreescritura de metodos del prototipo padre en el hijo:
+    acelerar() {
+        console.log("no hago sonidos");
+    }
+    motorUnico() {
+        console.log("No hay combustion interna");
+    }
+    //Ejemplo de metodo estatico:
+    static queSoy() {
+        console.log("Los autos eléctricos son aquellos cuyo motor es capaz de convertir la energía de una batería en energía mecánica");
+    }
+}
+
+//Ejecutando metodo estatico:
+Electrico.queSoy();
+
+const tesla = new Electrico("Model S", 2022, "%80");
+const nissan = new Carro("tsuru", 2017);
+
+console.log(tesla);
+console.log(nissan);
+
+tesla.acelerar();
+nissan.acelerar();
