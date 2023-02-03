bfe069d6e52ce86cb9997f9c94d59e4f2b9a2464
ulisesGutierrez <techwithulises@gmail.com>
2023-02-03 09:36:02 -0600

    ordenar

--- javascript/Colas.js
+++
@@ -1,8 +0,0 @@
-// Hacer cola
-
-/*
-En informática, una cola es una estructura de datos abstracta
-donde los elementos se mantienen en orden. Los elementos nuevos
-se pueden agregar al final de la cola y los elementos antiguos
-se quitan del frente de la cola.
-*/
