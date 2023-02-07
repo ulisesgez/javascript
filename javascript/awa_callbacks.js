ae062dd612bf1a85b77b5bcd253d18786302cb4a
ulisesGutierrez <techwithulises@gmail.com>
2023-02-06 22:55:51 -0600

    callbackhell

--- javascript/awa_callbacks.js
+++ javascript/awa_callbacks.js
@@ -66,3 +66,33 @@ frutas.forEach(frutas => {
 'uvas'
 'fresas'
 */
+
+//Otro ejemplo:
+
+function cuadradoCallback(value, callback) {
+    setTimeout(() => {
+        callback(value, value*value);
+    }, 0 | Math.random()*100);
+}
+
+//callbackhell
+
+cuadradoCallback(0, (value, result) => {
+    console.log("Inicia callback");
+    console.log(`Callback: ${value}, ${result}`);
+    cuadradoCallback(1, (value, result) => {
+        console.log(`Callback: ${value}, ${result}`);
+        cuadradoCallback(2, (value, result) => {
+            console.log(`Callback: ${value}, ${result}`);
+            cuadradoCallback(3, (value, result) => {
+                console.log(`Callback: ${value}, ${result}`);
+                cuadradoCallback(4, (value, result) => {
+                    console.log(`Callback: ${value}, ${result}`);
+                    cuadradoCallback(5, (value, result) => {
+                        console.log(`Callback: ${value}, ${result}`);
+                    });
+                });
+            });
+        });
+    });
+});
