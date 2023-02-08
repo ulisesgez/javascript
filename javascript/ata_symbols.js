5c0376f6b1e52ef7a948ccd8facbfb816b374608
ulisesGutierrez <techwithulises@gmail.com>
2023-02-07 22:49:36 -0600

    symbol

--- javascript/ata_symbols.js
+++ javascript/ata_symbols.js
@@ -11,21 +11,34 @@ let id4 = Symbol();//crea una referencia unica
 console.log(id === id2);//true
 
 //Ahora con symbol:
-
 console.log(id3 === id4);//false
 
+//identificador/referencia:
+
+let id5 = Symbol("id5");
+let id6 = Symbol("id6");
+
 //Objeto con symbol:
-
 const NOMBRE = Symbol();
-
 const persona = {
     //cuando queremos que sea un identificador unico dentro de las
     //propiedades de nuestro objeto y para eso utilizamos el symbol:
     [NOMBRE]: 'ulises'
 }
+
 console.log(persona);//{Symbol(): 'ulises'}
 
 persona.NOMBRE = 'ulises gutierrez'//{NOMBRE: ulises gutierrez, Symbol(): 'ulises'}
 
 console.log(persona.NOMBRE);//ulises gutierrez
 console.log(persona[NOMBRE]);//ulises
+
+//metodos, funciones:
+
+const SALUDAR = Symbol();
+
+persona[SALUDAR] = function() {
+    console.log('hola');
+}
+
+persona[SALUDAR]();
