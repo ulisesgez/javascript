879249bc3058a0b040bd0538f75a2a050427dd02
ulisesGutierrez <techwithulises@gmail.com>
2023-02-07 19:21:53 -0600

    asyncAwait

---
+++ javascript/aya_asyncAwait.js
@@ -0,0 +1,62 @@
+/*
+Van a esperar a que algo se cumpla para poder seguir ejecutando el proceso.
+*/
+function cuadradoPromise(value) {
+    if(typeof value !== "number") {
+        return Promise.reject(`Èrror, el valor ${value} no es un numero`);
+    }
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve({
+                value,
+                result: value * value
+            });
+        }, 0 | Math.random()*100);
+    });
+}
+/*
+async function funcionAsincronaDeclarada() {
+    try {
+        console.log("Inicio");
+        let obj = await cuadradoPromise(0);
+        console.log(`Promise: ${obj.value}, ${obj.result}`);
+        obj = await cuadradoPromise(1);
+        console.log(`Promise: ${obj.value}, ${obj.result}`);
+        obj = await cuadradoPromise(2);
+        console.log(`Promise: ${obj.value}, ${obj.result}`);
+        obj = await cuadradoPromise(3);
+        console.log(`Promise: ${obj.value}, ${obj.result}`);
+        obj = await cuadradoPromise(4);
+        console.log(`Promise: ${obj.value}, ${obj.result}`);
+        obj = await cuadradoPromise(5);
+        console.log(`Promise: ${obj.value}, ${obj.result}`);
+    }
+    catch(err) {
+        console.error(err);
+    }
+}
+
+funcionAsincronaDeclarada();
+*/
+
+const funcionAsincronaExpresada = async () => {
+    try {
+        console.log("Inicio");
+        let obj = await cuadradoPromise(6);
+        console.log(`Promise: ${obj.value}, ${obj.result}`);
+        obj = await cuadradoPromise(7);
+        console.log(`Promise: ${obj.value}, ${obj.result}`);
+        obj = await cuadradoPromise(8);
+        console.log(`Promise: ${obj.value}, ${obj.result}`);
+        obj = await cuadradoPromise("9");//error
+        console.log(`Promise: ${obj.value}, ${obj.result}`);
+        obj = await cuadradoPromise(10);
+        console.log(`Promise: ${obj.value}, ${obj.result}`);
+        obj = await cuadradoPromise(11);
+        console.log(`Promise: ${obj.value}, ${obj.result}`);
+    }
+    catch(err) {
+        console.error(err);
+    }
+}
+funcionAsincronaExpresada();
