4aa3ef09f3ada9462c62c3ca4a637e9bc152bddc
ulisesGutierrez <techwithulises@gmail.com>
2023-02-15 16:16:25 -0600

    actual

---
+++ javascript/acw_promiseAnyMétodosPrivados.js
@@ -0,0 +1,9 @@
+//si todo a ha sido reuelto:
+
+const promise1 = new Promise((resolve, reject) => reject('reject'));
+const promise2 = new Promise((resolve, reject) => resolve('resolve'));
+const promise3 = new Promise((resolve, reject) => resolve('resolve2'));
+
+//Nos devuelve la primera que cumpla:
+Promise.any([promise1, promise2, promise3])
+.then(response => console.log(response));//resolve
