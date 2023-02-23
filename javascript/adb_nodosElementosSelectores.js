b42ae76623fbe40b22f6554897d963b2fd8e5f8c
ulisesGutierrez <techwithulises@gmail.com>
2023-02-23 12:01:58 -0600

    comentarios

--- javascript/adb_nodosElementosSelectores.js
+++ javascript/adb_nodosElementosSelectores.js
@@ -1,11 +1,12 @@
-//No es un arreglo, es una coleccionde cuantos li tenemos:
-console.log(document.getElementsByTagName("li"));
+//No es un arreglo, es una coleccion de cuantos li tenemos:
+console.log(document.getElementsByTagName("li"));//HTMLCollection(5) [li, li, li, li, li]
 //Coleccion de elementos con la clase card, no lleva el punto:
-console.log(document.getElementsByClassName("card"));
+console.log(document.getElementsByClassName("card"));//HTMLCollection(5) [figure.card, figure.card, figure.card, figure.card, figure.card]
 //Input
-console.log(document.getElementsByName("nombre"));
+console.log(document.getElementsByName("nombre"));//NodeList [input]
 //Por id
-console.log(document.getElementById("menu"));
+console.log(document.getElementById("menu"));//Devuelve el nav
+//Query selector o consulta de selector:
 //Trae el elemento de acuerdo al selector:
 console.log(document.querySelector("#menu"));
 //Trae el primer elemento de acuerdo a los selectores:
@@ -22,7 +23,7 @@ document.querySelectorAll("a").forEach(element => console.log(element));
 Este ejemplo no abarca este html:
 En caso de ser un <div id= "micaja">soy una caja</div>
 con lo siguente en la consola obtenemos:
-*/
+
 let cajaUno = document.getElementById("micaja");//<div id= "micaja">soy una caja</div>
 let cajaDos = document.getElementById("micaja").innerHTML;//soy una caja
 cajaDos.innerHTML = "soy otra";//Modificamos el div a: soy otra caja
@@ -46,12 +47,14 @@ console.log(todosLosDivs);//array de elementos de los divs
 let contenidoEnTexto = todosLosDivs[2].textContent;
 console.log(contenidoEnTexto);//muestra el texto del div
 
-/*
+
 let contenidoEnTexto = todosLosDivs[2].innerHTML;
 console.log(contenidoEnTexto);//tambien muestra el texto del div
 
 let contenidoEnTexto.innerHTML = "otro texto";
 console.log(contenidoEnTexto);//cambiar el texto del div
-*/
+
 
 cambiaColor("green");
+
+*/
