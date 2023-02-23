34d5a5fca64a331ef3cbd3ccf6ddef21b0a60ed0
ulisesGutierrez <techwithulises@gmail.com>
2023-02-23 10:30:46 -0600

    dom

--- javascript/ada_dom.js
+++ javascript/ada_dom.js
@@ -1,48 +1,22 @@
-console.log('Elementos del documento|');
+console.log('Elementos del documento');
+//Esto:
 console.log(window.document);
-console.log(document);
-console.log(document.documentElement);//html
-console.log(document.head);
-console.log(document.body);
-console.log(document.doctype);//tipo de documento
-console.log(document.links);//encuentra links en la pagina
+//es equivalente a este, no es necesario el window:
+console.log(document);//Imprime el #document
+console.log(document.head);//Devuelve cabeza del html
+console.log(document.body);//Devuleve lo que hay en body
+console.log(document.html);//Esto genera undefined, no es como los anteriores.
+console.log(document.documentElement);//html, con esto si generamos el html.
+console.log(document.doctype);//Devuleve tipo de documento
+console.log(document.charset);//Juego de caracteres.
+console.log(document.title);//Devuelve el titulo
+console.log(document.links);//Encuentra links en la pagina
 console.log(document.images);//Encuentra imagenes en la pagina
+console.log(document.forms);//Devuelve los formularios.
 console.log(document.styleSheets);//css
 console.log(document.scripts);//javascript
-document.write("<h2>helloWorld!</h2>")
-/*
-En caso de ser un <div id= "micaja">soy una caja</div>
-con lo siguente en la consola obtenemos:
-*/
-let cajaUno = document.getElementById("micaja");//<div id= "micaja">soy una caja</div>
-let cajaDos = document.getElementById("micaja").innerHTML;//soy una caja
-cajaDos.innerHTML = "soy otra";//Modificamos el diav a: soy otra caja
-
-//conseguir elemento desde id concreto:
-
-cajaDos.style.background = "red";//fondo rojo
-cajaDos.style.padding = "20px";//padding 20 px
-cajaDos.style.color = "white";//Texto white
-cajaDos.className = "hola hola2"//cambiar el nombre de la clase
-const cajaDos = document.querySelector("#micaja");//seleccionar elemento
-
-function cambiaColor(color) {
-    cajaDos.style.background = color;
-}
-
-//conseguir elementos por su etiqueta:
-let todosLosDivs = document.getElementsByTagName('div');
-console.log(todosLosDivs);//array de elementos de los divs
-
-let contenidoEnTexto = todosLosDivs[2].textContent;
-console.log(contenidoEnTexto);//muestra el texto del div
-
-/*
-let contenidoEnTexto = todosLosDivs[2].innerHTML;
-console.log(contenidoEnTexto);//tambien muestra el texto del div
-
-let contenidoEnTexto.innerHTML = "otro texto";
-console.log(contenidoEnTexto);//cambiar el texto del div
-*/
-
-cambiaColor("green");
+//Despues de 3 segundos nos marcara el texto que hemos seleccionado:
+setTimeout(() => {
+    console.log(document.getSelection().toString());
+}, 3000);
+document.write("<h2>helloWorld!</h2>");//Se creara un h2 despues de todo enuestro contenido.
---
+++ javascript/add_ estilosVariablesCss.js
@@ -0,0 +1,61 @@
+const linkDom = document.querySelector(".link-dom");
+console.log(linkDom.style);
+/*
+CSSStyleDeclaration {0: 'color', 1: 'background-color', accentColor: '', additiveSymbols: '', alignContent: '', alignItems: '', alignSelf: '', …}
+0: "color"
+1: "background-color"
+*/
+console.log(linkDom.getAttribute("style"));//color: white; background-color: black;
+
+console.log(linkDom.style.backgroundColor);//black
+console.log(linkDom.style.color);//white
+
+console.log(window.getComputedStyle(linkDom));
+//No es necesario el window
+console.log(getComputedStyle(linkDom).getPropertyValue("color"));//rgb(255, 255, 255)
+
+linkDom.style.setProperty("text-decoration", "none");
+linkDom.style.setProperty("display", "block");
+linkDom.style.width = "50%";
+linkDom.style.textAlign = "center";
+linkDom.style.marginLeft = "auto";
+linkDom.style.marginRight = "auto";
+linkDom.style.borderRadius = ".5rem";
+
+console.log(linkDom.style);
+/*
+CSSStyleDeclaration {0: 'color', 1: 'background-color', 2: 'text-decoration-line', 3: 'text-decoration-thickness', 4: 'text-decoration-style', 5: 'text-decoration-color', 6: 'display', 7: 'width', 8: 'text-align', 9: 'margin-left', 10: 'margin-right', 11: 'border-top-left-radius', 12: 'border-top-right-radius', 13: 'border-bottom-right-radius', 14: 'border-bottom-left-radius', accentColor: '', additiveSymbols: '', alignContent: '', alignItems: '', alignSelf: '', …}
+0: "color"
+1: "background-color"
+2: "text-decoration-line"
+3: "text-decoration-thickness"
+4: "text-decoration-style"
+5: "text-decoration-color"
+6: "display"
+7: "width"
+8: "text-align"
+9: "margin-left"
+10: "margin-right"
+11: "border-top-left-radius"
+12: "border-top-right-radius"
+13: "border-bottom-right-radius"
+14: "border-bottom-left-radius"
+*/
+
+//Variables CSS - Custom Propieries CSS:
+//documentElement representa/accedemos html
+//tiene $ al inicio por que son referencias:
+const $html = document.documentElement,
+$body = document.body;
+
+let varDarkColor = getComputedStyle($html).getPropertyValue("--dark-color"),
+ varYellowColor = getComputedStyle($html).getPropertyValue("--yellow-color");
+
+console.log(varDarkColor, varYellowColor);
+
+$body.style.backgroundColor = varDarkColor;
+$body.style.color = varYellowColor;
+
+$html.style.setProperty("--dark-color", "#000");
+varDarkColor = getComputedStyle($html).getPropertyValue("--dark-color");
+$body.style.setProperty("background-color", varDarkColor);
