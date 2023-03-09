1a2c1487630415b571d1c7f38adac0d23b21fbfc
ulisesGutierrez <techwithulises@gmail.com>
2023-03-08 20:37:49 -0600

    ajax

---
+++ javascriptAjax/adt_ajaxIntroduccion.js
@@ -0,0 +1,20 @@
+ /*
+Ajax se puede comunicar coin el servidor, intercambiar datos,
+actualizar la interfaz del usuario sin necesidad de recargar el navegador.
+
+Estados de petición:
+
+READY_STATE_UNINITIALIZED = 0
+READY_STATE_LOADING = 1
+READY_STATE_LOADED = 2
+READY_STATE_INTERACTIVE = 3
+READY_STATE_COMPLETE = 4
+
+Codigos de estado de respuesta HTTP:
+
+Respuestas informativas (100–199),
+Respuestas satisfactorias (200–299),
+Redirecciones (300–399),
+Errores de los clientes (400–499),
+y errores de los servidores (500–599).
+ */
