ad7343d42d8e7f6b70d3bcd5c9a4e8fdb040f060
ulisesGutierrez <techwithulises@gmail.com>
2023-03-13 17:31:10 -0600

    axios

---
+++ javascriptAjax/adx_axios.js
@@ -0,0 +1,24 @@
+(() => {
+    const $axios = document.getElementById("axios"),
+    $fragment = document.createDocumentFragment();
+
+    axios.get("https://api.escuelajs.co/api/v1/products")
+    .then((res) => {
+        //console.log(res);
+        let json = res.data;
+        json.forEach(el => {
+            const $li = document.createElement("li");
+            $li.innerHTML = `${el.id} -- ${el.title}`
+            $fragment.appendChild($li);
+        });
+        $axios.appendChild($fragment);
+    })
+    .catch((err) => {
+        console.log(err.response);
+        let message = err.response.statusText || "error";
+        $axios.innerHTML = `error ${err.response.status}: ${message}`;
+    })
+    .finally(() => {
+        console.log("ejcutado independientemente del resultado.");
+    })
+})();
