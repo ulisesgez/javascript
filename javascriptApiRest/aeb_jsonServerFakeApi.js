c96220ae83f747ccff69c1f05e21d77ca126932d
ulisesGutierrez <techwithulises@gmail.com>
2023-03-15 22:25:44 -0600

    jsonServer

---
+++ javascriptApiRest/aeb_jsonServerFakeApi.js
@@ -0,0 +1,8 @@
+//Instalar nodejs
+//Ir a https://jsonplaceholder.typicode.com/
+//O ir directamente a https://github.com/typicode/json-server
+//Install JSON Server ejemplo: npm install -g json-server
+//Crear el archivo json como db.json
+//Ejecutar estando dentro en la terminal:
+//json-server --watch db.json
+//Nos permitira verlo en el navegador
