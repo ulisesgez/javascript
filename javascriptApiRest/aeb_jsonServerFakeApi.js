3e571b93b003c0f580fe882e339db4c618c299b9
ulisesGutierrez <techwithulises@gmail.com>
2023-03-16 17:30:39 -0600

    rename

--- javascriptApiRest/aeb_jsonServerFakeApi.js
+++ javascriptApiRest/aeb_jsonServerFakeApi.js
@@ -1,8 +1,12 @@
-//Instalar nodejs
-//Ir a https://jsonplaceholder.typicode.com/
-//O ir directamente a https://github.com/typicode/json-server
-//Install JSON Server ejemplo: npm install -g json-server
-//Crear el archivo json como db.json
-//Ejecutar estando dentro en la terminal:
-//json-server --watch db.json
-//Nos permitira verlo en el navegador
+/*
+Instalar nodejs
+Ir a https://jsonplaceholder.typicode.com/
+O ir directamente a https://github.com/typicode/json-server
+Install JSON Server ejemplo: npm install -g json-server
+Crear el archivo json como db.json
+Ejecutar estando dentro en la terminal:
+json-server --watch db.json
+En nuestro caso:
+json-server --watch aeb_jsonServerFakeApi
+Nos permitira verlo en el navegador
+*/
