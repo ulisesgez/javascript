2b5a9b87758acc47bb9a14a63c7b6d8ecda72a45
ulisesGutierrez <techwithulises@gmail.com>
2023-03-28 21:34:45 -0600

    http

---
+++ javascriptApiRest/aea_apiRestIntroduction.js
@@ -0,0 +1,14 @@
+/*
+¿Qué son los HTTP Status Codes?
+Son lña forma en que el protocolo http nos da un spoiler de que paso con nuestra solicitud,
+si va todo bien, si va todo mal, por que si todo puede salir mal, el internet
+esta roto y nuestras aplicaciones pueden tener muchos fallos. Tenemos que estar preparados
+paar reaccionar y decirle a los usuarios que esta pasando, si fallo la api, si fallamos
+nosotros, si fallaron ellos, etc.Los HTTP Status Codes nos ayudan a saber reaccionar a este
+tipo de situaciones.
+
+2xx ---> va todo bien
+3xx ---> redirects, la ruta a la que estamos entramos no es la ruta final
+4xx ---> algo va mal, el frontend fallo
+5xx ---> algo va mal, el backend fallo
+*/
