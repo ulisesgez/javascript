40c6c81195525f56ea252f7b21d99e5a846a23bd
ulisesGutierrez <techwithulises@gmail.com>
2023-03-30 22:25:15 -0600

    api

--- npm/conceptos.js
+++ npm/conceptos.js
@@ -96,4 +96,15 @@ npm list
 
 Ver lista de paquetes globlales
 npm list -g
+
+Instalación de dependencias de versiones específicas
+
+npm install package-name -o → Instalar una dependencia opcional
+Se pueden generar conflictos cuando se tienen paquetes que usan la misma dependencia pero en
+versiones diferentes. Para evitar esto se puede simular una instalación con npm install
+package-name —dry-run. Con esto se simula la instalación pero sin agregar ningún paquete,
+si no hay ningún conflicto se procede a instalar de la manera convencional.
+npm install package-name@0.15.0 → Instalar la versión especifica de un paquete. Si luego se
+quiere instalar la versión más reciente se usa npm install package-name@latest.
+npm install → Instala las dependencias que estén dentro de un package.json.
 */
