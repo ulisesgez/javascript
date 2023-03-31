8bbadd14b7e5374e1f78ff51047296d4cf41999e
ulisesGutierrez <techwithulises@gmail.com>
2023-03-30 21:48:21 -0600

    npm

---
+++ npm/conceptos.js
@@ -0,0 +1,17 @@
+/*
+NPM:
+Es el acrónimo de Node Package Manager. Es un sistema de gestión de paquetes (o “módulos”)
+para el lenguaje de programación JavaScript, especialmente diseñado para ser utilizado con el
+entorno de ejecución JavaScript Node.js. Sirve para facilitar la instalación, actualización y
+eliminación de librerías y herramientas de código abierto que son útiles para desarrollar
+aplicaciones JavaScript. Además, permite a los desarrolladores compartir y reutilizar código,
+ahorrando tiempo y esfuerzo en el desarrollo de sus proyectos.
+
+Un sistema de gestión de paquetes es una herramienta informática que se encarga de automatizar
+el proceso de instalación, actualización y eliminación de programas o librerías en un sistema
+operativo. Estos paquetes suelen contener código fuente o ejecutable, así como documentación
+y otros recursos necesarios para su funcionamiento. Los sistemas de gestión de paquetes
+permiten a los desarrolladores y administradores de sistemas instalar, actualizar y desinstalar
+fácilmente software en sus sistemas, ahorrando tiempo y esfuerzo, y asegurando la
+compatibilidad entre las diferentes dependencias de los paquetes.
+*/
