755a397f451dbcface719a8e2218e54444ebdb9c
ulisesGutierrez <techwithulises@gmail.com>
2023-04-11 22:30:19 -0600

    reduce

--- javascript/aco_flatMapTrimStartTrimEnd.js
+++ javascript/aco_flatMapTrimStartTrimEnd.js
@@ -151,3 +151,18 @@ result1 // 'hola'
 result2 // 'hola      '
 result3 // '      hola'
 */
+
+/*
+REDUCE, como lo dice su nombre reduce a un solo valor y no devuelve otro array,
+simplemente un valor.
+Se utiliza muchísimo para hacer cálculos a partir de la información de un array.
+En su composición, a primeras, tiene como argumentos de la función del primer
+parámetro, al acumulador y como segundo parámetro al elemento por el que va
+iterando el loop. Y como segundo argumento del reduce(), se pasa el valor
+inicial del acumulador.
+*/
+const totales = [1, 2, 3, 4];
+let sum = 0;
+const reducir = totales.reduce((sum, element) => sum + element, 0)
+//acumulado:
+console.log(reducir);//10
