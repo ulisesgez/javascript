54b0b081138220f1b0d2e833d781ec83ce60d787
ulisesGutierrez <techwithulises@gmail.com>
2023-05-20 21:55:26 -0600

    introduccion

---
+++ javascript/aaa_introduccion.js
@@ -0,0 +1,47 @@
+/*
+¿Qué es JavaScript?
+JavaScript se introdujo en 1995 como una forma de agregar programas a páginas web en el
+navegador Netscape Navigator. El lenguaje ha sido desde entonces adoptado por todos los otros
+navegadores web principales. Ha hecho que las aplicaciones web modernas sean posibles:
+aplicaciones con las que puedes interactuar directamente, sin hacer una recarga de página
+para cada acción. JavaScript también es utilizado en sitios web más tradicionales para
+proporcionar diversas formas de interactividad e ingenio. Es importante tener en cuenta que
+JavaScript casi no tiene nada que ver con el lenguaje de programación llamado Java.
+El nombre similar fue inspirado por consideraciones de marketing, en lugar de buen juicio.
+Cuando JavaScript estaba siendo introducido, el lenguaje Java estaba siendo fuertemente
+comercializado y estaba ganando popularidad. Alguien pensó que era una buena idea intentar
+cabalgar sobre este éxito. Ahora estamos atrapados con el nombre.
+Después de su adopción fuera de Netscape, un documento estándar fue escrito para describir
+la forma en que debería funcionar el lenguaje JavaScript, para que las diversas piezas de
+software que decían ser compatibles con JavaScript en realidad estuvieran hablando del mismo
+lenguaje. Este se llamo el Estándar ECMAScript, por Ecma International que hizo la
+estandarización. En la práctica, los términos ECMAScript y JavaScript se puede usar
+indistintamente, son dos nombres para el mismo lenguaje.
+Hay quienes dirán cosas terribles sobre JavaScript. Muchas de estas cosas son verdaderas.
+Cuando estaba comenzando a escribir algo en JavaScript por primera vez, rápidamente comencé
+a despreciarlo. El lenguaje aceptaba casi cualquier cosa que escribiera, pero la interpretaba
+de una manera que era completamente diferente de lo que quería decir. Por supuesto, esto
+tenía mucho que ver con el hecho de que no tenía idea de lo que estaba haciendo, pero hay
+un problema real aquí: JavaScript es ridículamente liberal en lo que permite.
+La idea detrás de este diseño era que haría a la programación en JavaScript más fácil para
+los principiantes. En realidad, lo que mas hace es que encontrar problemas en tus programas
+sea más difícil porque el sistema no los señalará por ti.
+Sin embargo, esta flexibilidad también tiene sus ventajas. Deja espacio para muchas técnicas
+que son imposibles en idiomas más rígidos, y como verás (por ejemplo en el Capítulo 10) se
+pueden usar para superar algunas de las deficiencias de JavaScript. Después de aprender el
+idioma correctamente y luego de trabajar con él por un tiempo, he aprendido a querer a JavaScript.
+Ha habido varias versiones de JavaScript. ECMAScript versión 3 fue la versión mas ampliamente
+compatible en el momento del ascenso de JavaScript a su dominio, aproximadamente entre 2000
+y 2010. Durante este tiempo, se trabajó en marcha hacia una ambiciosa versión 4, que planeaba
+una serie de radicales mejoras y extensiones al lenguaje. Cambiar un lenguaje vivo y
+ampliamente utilizado de una manera tan radical resultó ser políticamente difícil y el
+trabajo en la versión 4 fue abandonado en 2008, lo que llevó a la versión 5, mucho menos
+ambiciosa, que se publicaría en el 2009. Luego, en 2015, una actualización importante,
+incluyendo algunas de las ideas planificadas para la versión 4, fue realizada.
+Desde entonces hemos tenido actualizaciones nuevas y pequeñas cada año.
+El hecho de que el lenguaje esté evolucionando significa que los navegadores deben mantenerse
+constantemente al día, y si estás usando uno más antiguo, puede que este no soporte todas las
+mejoras. Los diseñadores de lenguajes tienen cuidado de no realizar cualquier cambio que pueda
+romper los programas ya existentes, de manera que los nuevos navegadores puedan todavía
+ejecutar programas viejos.
+*/
