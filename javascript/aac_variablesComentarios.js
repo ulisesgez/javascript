0c86cf62ee1f9d07f330955679a94326e0a372ce
ulisesGutierrez <techwithulises@gmail.com>
2023-05-27 16:19:21 -0600

    text

--- javascript/aac_variablesComentarios.js
+++ javascript/aac_variablesComentarios.js
@@ -1,53 +1,157 @@
 /*
 Comentarios
-Los comentarios se comportan como espacios en blanco y se descartan durante la ejecución del script.
-La sintaxis de los comentarios es la misma que en C++ y en muchos otros lenguajes:
+Los comentarios se comportan como espacios en blanco y se descartan durante la ejecución del
+script. La sintaxis de los comentarios es la misma que en C++ y en muchos otros lenguajes:
 */
+
 // a one line comment
 
 /* this is a longer,
  * multi-line comment
  */
+
 /*
-No puede anidar comentarios de bloque. Esto sucede a menudo cuando accidentalmente incluye un asterisco - diagonal secuencia en su comentario, lo que terminará el comentario.
-En este caso, debe romper el asterisco - diagonal patrón. Por ejemplo, al insertar una barra invertida:
+No puede anidar comentarios de bloque. Esto sucede a menudo cuando accidentalmente incluye un
+asterisco - diagonal secuencia en su comentario, lo que terminará el comentario.
+En este caso, debe romper el asterisco - diagonal patrón. Por ejemplo, al insertar una barra
+invertida:
 */
+
 /* You can /* nest comments *\/ by escaping slashes */
 
 /*
 Declaraciones
 JavaScript tiene tres tipos de declaraciones de variables.
 
-var
+var:
 Declara una variable, opcionalmente inicializándola a un valor.
 
-let
+let:
 Declara una variable local con ámbito de bloque y, opcionalmente, la inicializa en un valor.
 
-const
+const:
 Declara una constante con nombre de solo lectura y ámbito de bloque.
 
 Variables:
-Utiliza variables como nombres simbólicos para valores en su aplicación. Los nombres de las variables, llamados identificadores , se ajustan a ciertas reglas.
+Utiliza variables como nombres simbólicos para valores en su aplicación. Los nombres de las
+variables, llamados identificadores , se ajustan a ciertas reglas.
 
-Un identificador de JavaScript generalmente comienza con una letra, un guión bajo ( _) o un signo de dólar ( $). Los caracteres subsiguientes también pueden ser dígitos ( 0- 9).
-Dado a que JavaScript distingue entre mayúsculas y minúsculas, las letras incluyen los caracteres Ahasta Z(mayúsculas) y ahasta z(minúsculas).
-
-Puedes utilizar la mayoría de las letras ISO 8859-1 o Unicode, como åy üen los identificadores. (Para obtener más detalles, consulte esta publicación de blog o la referencia de gramática léxica ). También puede usar las secuencias de escape Unicode como caracteres en los identificadores.
+Un identificador de JavaScript generalmente comienza con una letra, un guión bajo ( _) o un
+signo de dólar ( $). Los caracteres subsiguientes también pueden ser dígitos ( 0- 9).
+Dado a que JavaScript distingue entre mayúsculas y minúsculas, las letras incluyen los
+caracteres Ahasta Z(mayúsculas) y ahasta z(minúsculas). Puedes utilizar la mayoría de las
+letras ISO 8859-1 o Unicode, como åy üen los identificadores. (Para obtener más detalles,
+consulte esta publicación de blog o la referencia de gramática léxica ).
+También puede usar las secuencias de escape Unicode como caracteres en los identificadores.
 
 Algunos ejemplos de nombres legales son Number_hits, temp99, $credity _name.
 
-Declaración de variables
+Declaración de variables:
 Puede declarar una variable de dos maneras:
 
-Con la palabra clave var. Por ejemplo, var x = 42. Esta sintaxis se puede utilizar para declarar variables locales y globales , según el contexto de ejecución .
-Con la palabra clave consto let. Por ejemplo, let y = 13. Esta sintaxis se puede utilizar para declarar una variable local de ámbito de bloque. (Consulte Alcance variable a continuación).
-Puede declarar variables para desempaquetar valores utilizando la sintaxis de asignación de desestructuración . Por ejemplo, const { bar } = foo. Esto creará una variable nombrada bary le asignará el valor correspondiente a la clave del mismo nombre de nuestro objeto foo.
+> Con la palabra clave var. Por ejemplo, var x = 42. Esta sintaxis se puede utilizar para
+declarar variables locales y globales , según el contexto de ejecución.
 
-Las variables siempre deben declararse antes de que se utilicen. JavaScript solía permitir la asignación a variables no declaradas, lo que crea una variable global no declarada . Este es un error en modo estricto y debe evitarse por completo.
+> Con la palabra clave consto let. Por ejemplo, let y = 13. Esta sintaxis se puede utiliza
+para declarar una variable local de ámbito de bloque. (Consulte Alcance variable a continuación).
 
+También puedes simplemente asignar un valor a una variable. Por ejemplo, x = 42.
+Este formulario crea una variable global no declarada. También genera una advertencia estricta
+de JavaScript. Las variables globales no declaradas a menudo pueden provocar un comportamiento
+inesperado. Por lo tanto, se desaconseja utilizar variables globales no declaradas.
+
+Puede declarar variables para desempaquetar valores utilizando la sintaxis de asignación de
+desestructuración . Por ejemplo, const { bar } = foo. Esto creará una variable nombrada bary
+le asignará el valor correspondiente a la clave del mismo nombre de nuestro objeto foo.
+Las variables siempre deben declararse antes de que se utilicen. JavaScript solía permitir la
+asignación a variables no declaradas, lo que crea una variable global no declarada.
+Este es un error en modo estricto y debe evitarse por completo.
+
+Evaluar variables:
+Una variable declarada usando la instrucción var o let sin un valor asignado especificado
+tiene el valor de undefined.
+Un intento de acceder a una variable no declarada da como resultado el disparo de una
+excepción ReferenceError:
+*/
+var a;
+console.log('El valor de a es ' + a); // El valor de a es undefined
+
+console.log('El valor de b es ' + b); // El valor de b es undefined
+var b;
+// Esto puede desconcertarte hasta que leas 'Elevación de variable' a continuación
+
+console.log('El valor de c es ' + c); // Error de referencia no detectado: c no está definida
+
+let x;
+console.log('El valor de x es ' + x); // El valor de x es undefined
+
+console.log('El valor de y es ' + y); // Error de referencia no detectada: y no está definida
+let y;
+
+/*
+Puedes usar undefined para determinar si una variable tiene un valor. En el siguiente código,
+a la variable input no se le asigna un valor y la declaración if evalúa a true.
+*/
+var input;
+if (input === undefined) {
+  doThis();
+} else {
+  doThat();
+}
+/*
+El valor undefined se comporta como false cuando se usa en un contexto booleano.
+Por ejemplo, el siguiente código ejecuta la función myFunction porque el elemento myArray
+es undefined:
+*/
+var myArray = [];
+if (!myArray[0]) myFunction();
+/*
+El valor undefined se convierte en NaN cuando se usa en contexto numérico.
+*/
+var a;
+a + 2;  // Evalúa a NaN
+
+/*
+Cuando evalúas una variable null, el valor nulo se comporta como 0 en contextos numéricos
+y como false en contextos booleanos. Por ejemplo:
+*/
+var n = null;
+console.log(n * 32); // Registrará 0 en la consola
+
+/*
+Ámbito de variables
+Cuando declaras una variable fuera de cualquier función, se denomina variable global,
+porque está disponible para cualquier otro código en el documento actual. Cuando declaras una
+variable dentro de una función, se llama variable local, porque solo está disponible dentro
+de esa función.
+
+JavaScript anterior a ECMAScript 2015 no tiene el ámbito de la declaración de bloque.
+Más bien, una variable declarada dentro de un bloque es local a la función (o ámbito global)
+en el que reside el bloque.
+
+Por ejemplo, el siguiente código registrará 5, porque el ámbito de x es el contexto global
+(o el contexto de la función si el código es parte de una función). El ámbito de x no se
+limita al bloque de instrucciones if inmediato.
+*/
+if (true) {
+    var a = 5;
+  }
+  console.log(a);  // a es 5
+
+/*
+Este comportamiento cambia cuando se usa la declaración let (introducida en ECMAScript 2015).
+*/
+if (true) {
+    let b = 5;
+  }
+  console.log(b); // ReferenceError: y no está definida
+/*
 Declaración e inicialización
-En una declaración como let x = 42, la let xparte se llama declaración y la = 42parte se llama inicializador . La declaración permite acceder a la variable más tarde en el código sin lanzar un ReferenceError, mientras que el inicializador asigna un valor a la variable. En las declaraciones vary let, el inicializador es opcional. Si una variable se declara sin inicializador, se le asigna el valor undefined.
+En una declaración como let x = 42, la let xparte se llama declaración y la = 42parte se
+llama inicializador . La declaración permite acceder a la variable más tarde en el código
+sin lanzar un ReferenceError, mientras que el inicializador asigna un valor a la variable.
+En las declaraciones vary let, el inicializador es opcional. Si una variable se declara sin
+inicializador, se le asigna el valor undefined.
 
 let x;
 console.log(x); // logs "undefined"
@@ -55,7 +159,6 @@ Copiar al portapapeles
 En esencia, let x = 42es equivalente a let x; x = 42.
 
 constlas declaraciones siempre necesitan un inicializador, porque prohíben cualquier tipo de asignación después de la declaración, y es probable que inicializarlas implícitamente undefinedsea un error del programador.
-
 
 */
 
