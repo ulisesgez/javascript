bc9c53709a17e5331a402a980aaf92de6606828a
ulisesGutierrez <techwithulises@gmail.com>
2023-06-28 17:10:34 -0600

    switch

--- javascript/aav_condicionalesEstructurasControl.js
+++ javascript/aav_condicionalesEstructurasControl.js
@@ -327,3 +327,31 @@ console.log(contarCartas("K"));//1 Apostar
 console.log(contarCartas("A"));//0 Esperar
 
 //Otro ejemplo:
+
+const diaHoy = 0;
+
+switch (diaHoy) {
+    case 0:
+        console.log("Domingo");
+        break;
+    case 1:
+        console.log("Lunes");
+        break;
+    case 2:
+        console.log("Martes");
+        break;
+    case 3:
+        console.log("Miercoles");
+        break;
+    case 4:
+        console.log("Jueves");
+        break;
+    case 5:
+        console.log("Viernes");
+        break;
+    case 6:
+        console.log("Sabado");
+        break;
+    default:
+        console.log("No es un dia de la semana");
+}
