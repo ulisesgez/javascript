58a87cc4fd5619f8ca7a659308f011b61d4274db
ulisesGutierrez <gu362937@uaeh.edu.mx>
2023-10-10 15:26:22 -0600

    paradigmas

---
+++ javascript/abd_poo.js
@@ -0,0 +1,28 @@
+/*
+Programación Orientada a Objetos:
+La programación orientada a objetos
+
+Maquina abstracta:
+Computadora digital que ejecuta algoritmos los cuales estan formalizados 
+para que la maquina entienda.Puede realizar operaciones para:
+- procesar datos primitivos
+- controlar secuencia de ejecución de operaciones
+- controlar transferencia de datos
+- Manejo de memoria
+
+Lenjuages de programación:
+Es un formalismo artificial en el cual los algoritmos pueden ser expresados.
+
+Niveles de descripción:
+- Gramatica: ¿Que oraciones son correctas?
+- Semantica: ¿Que significa una oración correcta?
+- Sintaxis: ¿Como se escribe una oración correcta?
+- Pragmatica: ¿Como utilizamos una oración significativa?
+- Implementación: ¿Como se ejecuta una oración significativa?
+
+Paradigmas de programación:
+Teoria o conjunto de teorias cuyo nucleo central se acepta sin cuestionar
+y que suministra la base y modelo para resolver problemas y avanzar en el conocimiento.
+
+
+*/
