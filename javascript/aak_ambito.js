3651954e41e8cc03fdbbebc2d015eef4dec5837a
ulisesGutierrez <gu362937@uaeh.edu.mx>
2023-10-09 18:23:32 -0600

    varVsLet

--- javascript/aak_ambito.js
+++ javascript/aak_ambito.js
@@ -1,4 +1,25 @@
 /*
+Ambito de las variables:
+
+Las variables pueden ser globales o locales, esto depende de donde se declaren.
+Las variables globales son aquellas que se declaran fuera de una función y estan
+disponibles dentro y fuera de las funciones.
+*/
+
+var texto = "Hola mundo soy una variable global"
+var numero = 12;
+console.log(texto);
+console.log(numero);
+
+function holaMundo(texto) {
+    console.log(texto);
+    console.log(numero);
+    var hola_mundo = "Texto dentro de funcion"
+    console.log(hola_mundo);
+}
+holaMundo(texto);
+
+/*
 ¡Que pasaria si quisieramos acceder a numero dentro de la función?
 function holaMundo(texto) {
     console.log(texto);
