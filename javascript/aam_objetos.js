c651773e909a01db0e05fd8326f0e90b8b276885
ulisesGutierrez <gu362937@uaeh.edu.mx>
2023-10-14 18:42:21 -0600

    objetos

--- javascript/aam_objetos.js
+++ javascript/aam_objetos.js
@@ -558,3 +558,60 @@ console.log(tvOne.caracteristicas(50));
 let arreglo = [40];
 console.log(tvOne.caracteristicas.apply(tvTwo, arreglo));
 //console.log(tvOne.caracteristicas.apply(tvTwo, [40]));//tambien se ppuede pasar asi
+
+/*
+Ojetos literales:
+Nueva forma de escribir atributos y metodos
+*/
+
+//Antes:
+
+let nombre = 'pupi',
+    edad = 7;
+
+const perro = {
+    nombre: nombre,
+    edad: edad,
+    ladrar: function () {
+        console.log('wof wof!');
+    }
+}
+
+console.log(perro);
+perro.ladrar();
+
+//Ahora:
+
+const dog = {
+    nombre,
+    edad,
+    ladrar () {
+        console.log('guau guau!');
+    }
+}
+
+console.log(dog);
+dog.ladrar();
+
+//En funciones (antes):
+
+/*
+function newUser(user, age, country) {
+    return {
+        user: user,
+        age: age,
+        country: country
+    }
+}
+Ahora:
+*/
+
+function newUser(user, age, country) {
+    return {
+        user,
+        age,
+        country
+    }
+}
+
+console.log(newUser("yo", 20, "MX"));// { user: 'yo', age: 20, country: 'MX' }
