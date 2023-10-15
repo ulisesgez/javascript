ddca843f82f5edb54efb8d74607b020e4e98aa97
ulisesGutierrez <gu362937@uaeh.edu.mx>
2023-10-14 19:19:06 -0600

    obj

--- javascript/aam_objetos.js
+++ javascript/aam_objetos.js
@@ -8,7 +8,7 @@ let a = new String("Hola");
 const b = {}
 console.log(b);
 const c = new Object();
-console.log(c);
+console.log(c);//{}
 //dentro de un objeto a las variables se le van a llamar atributos/propiedades y a las funciones se les llama métodos */
 const jon = {
   nombre: "Jon",
@@ -22,30 +22,30 @@ const jon = {
     movil: "5215512345678"
   },
   saludar: function () {
-    console.log(`Hola :)`)
+    console.log(`Hola :)`)//template string
   },
   decirMiNombre: function () {
-    console.log(`Hola me llamo ${this.nombre} ${this.apellido} y tengo ${this.edad} años y me puedes seguir como ${this.contacto.twitter} en twitter.`)
+    console.log(`Hola me llamo ${this.nombre} ${this.apellido} y tengo ${this.edad} años y me puedes seguir como ${this.contacto.twitter} en twitter.`)//template string
   }
 }
 
-console.log(jon);
-console.log(jon["nombre"]);
-console.log(jon["apellido"]);
-console.log(jon.nombre);
-console.log(jon.apellido);
-console.log(jon.edad);
-console.log(jon.soltero);
-console.log(jon.pasatiempos);
-console.log(jon.pasatiempos[1]);
-console.log(jon.contacto);
-console.log(jon.contacto.twitter);
-jon.saludar();
-jon.decirMiNombre();
-console.log(Object.keys(jon));
-console.log(Object.values(jon));
-console.log(jon.hasOwnProperty("nombre"));
-console.log(jon.hasOwnProperty("nacimiento"));
+console.log(jon);//{nombre: 'Jon', apellido: 'MirCha', edad: 35, pasatiempos: ['Correr', 'Hacer ejercicio', 'Dar clases'], soltero: false, …}
+console.log(jon["nombre"]);//Jon
+console.log(jon["apellido"]);//MirCha
+console.log(jon.nombre);//Jon
+console.log(jon.apellido);//MirCha
+console.log(jon.edad);//35
+console.log(jon.soltero);//false
+console.log(jon.pasatiempos);//['Correr', 'Hacer ejercicio', 'Dar clases']
+console.log(jon.pasatiempos[1]);//Hacer ejercicio
+console.log(jon.contacto);//{email: 'jonmircha@gmail', twitter: '@jonmircha', movil: '5215512345678'}
+console.log(jon.contacto.twitter);//@jonmircha
+jon.saludar();//Hola :)
+jon.decirMiNombre();//Hola me llamo Jon MirCha y tengo 35 años y me puedes seguir como @jonmircha en twitter.
+console.log(Object.keys(jon));//['nombre', 'apellido', 'edad', 'pasatiempos', 'soltero', 'contacto', 'saludar', 'decirMiNombre']
+console.log(Object.values(jon));//['Jon', 'MirCha', 35, ['Correr', 'Hacer ejercicio', 'Dar clases'], false, {…}, ƒ, ƒ]
+console.log(jon.hasOwnProperty("nombre"));//true
+console.log(jon.hasOwnProperty("nacimiento"));//false
 
 //Otro ejemplo:
 
