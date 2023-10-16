bf6cd8d9e360472b904f428db58c7bdbba16d369
ulisesGutierrez <gu362937@uaeh.edu.mx>
2023-10-16 17:18:36 -0600

    rest

--- javascript/aaz_objetoConsole.js
+++ javascript/aaz_objetoConsole.js
@@ -65,3 +65,37 @@ let x = 1,//Si lo remplazamos por ejemplo con un 3 marca error e imprime el mens
     testXY = "Se espera que X siempre sea menor que Y";
 
 console.assert(x < y, {x, y, testXY});
+
+console.log(Math);//Imprime los metodos disponibles del objeto
+console.log(Math.PI);//3.1416
+console.log(Math.abs(7.8));//Valor absoluto de un numero
+console.log(Math.abs(-7.8));//7.8
+console.log(Math.ceil(7.8));//Redondea hacia arriba 8
+console.log(Math.floor(7.8));//Redondea hacia abajo 7
+console.log(Math.round(7.49));//7
+console.log(Math.sqrt(81));//Raiz cuadrada 9
+console.log(Math.pow(2,5));//Potencia, 2 a la quinta = 32
+console.log(Math.sign(-7.8));//Positivo, negativo, cero -1 0 1
+console.log(Math.random());//valor aleatorio entre 0 y 1
+console.log(Math.random()*1000);//valor aleatorio entre 0 y 1000
+
+console.log(Date);//Imprime varios datos, entre elos la fecha, hora, region
+
+let fecha = new Date();
+console.log(fecha.getDate());//Imprime dia del mes.
+console.log(fecha.getDay());//Numero del dia de la semana, domingo -> 0.
+console.log(fecha.getMonth());//NUmero del mes
+console.log(fecha.getFullYear());//Año actual
+console.log(fecha.getHours());//Hora de 0 a 24
+console.log(fecha.getMinutes());// Minutos
+console.log(fecha.getSeconds());// Segundos
+console.log(fecha.getMilliseconds());// Milisegundos
+
+//Formas mas humanas:
+
+console.log(fecha.toString());
+console.log(fecha.toDateString());
+console.log(fecha.toLocaleString());
+console.log(fecha.toLocaleDateString());
+console.log(fecha.toLocaleTimeString());
+console.log(fecha.getTimezoneOffset());
