d1bc712a33db9a8147161040e8777ee1663e3c2e
ulisesGutierrez <gu362937@uaeh.edu.mx>
2023-10-21 12:10:00 -0600

    prototipos

--- javascript/aaw_prototipos.js
+++ javascript/aaw_prototipos.js
@@ -1,5 +1,56 @@
-//Función constructora:
+//Objeto literal y prototipos:
+const natalia = {
+    nombre: "Natalia",
+    edad: 20,
+    cursosAprobados: [
+        "Curso Definitivo de HTML y CSS",
+        "Curso Practico de HTML y CSS"
+    ],
+    aprobarCurso(nuevoCurso) {
+        this.cursosAprobados.push(nuevoCurso);
+    }
+};
+natalia.cursosAprobados.push("Curso de Responsive Design");
+console.log(natalia);//Agregamos un nuevo curso al objeto literal
 
+natalia.edad += 1;
+console.log(natalia);//Agregamos un año a la edad del objeto literal
+
+console.log(natalia.cursosAprobados);//Imprimimos los cursos aprobados del objeto literal
+natalia.aprobarCurso("Curso de JavaScript");
+console.log(natalia);//Agregamos un nuevo curso al objeto literal
+
+//Prototipos:
+function Student(nombre, edad, cursosAprobados) {
+    this.nombre = nombre;
+    this.edad = edad;
+    this.cursosAprobados = cursosAprobados;
+    /*
+    this.aprobarCurso = function(nuevoCurso) {
+        this.cursosAprobados.push(nuevoCurso);
+    }
+    */
+}
+//Agregamos un metodo al prototipo de la funcion constructora:
+Student.prototype.aprobarCurso = function(nuevoCurso) {
+    this.cursosAprobados.push(nuevoCurso);
+}
+
+//Creamos un nuevo objeto a partir de la funcion constructora:
+const juanita = new Student(
+    "Juanita Alejandra",
+    15,
+    [
+        "Curso de Introduccion a la Produccion de Videojuegos",
+        "Curso de Programacion Basica"
+    ]
+);
+
+console.log(juanita);//Imprimimos el objeto creado a partir de la funcion constructora.
+juanita.aprobarCurso("Curso de Videojuegos en Unity");
+console.log(juanita);//Agregamos un nuevo curso al objeto creado a partir de la funcion constructora.
+
+//Otra forma de crear un prototipo:
 function Animal(nombre, genero) {
     //Atributos:
     this.nombre = nombre;
