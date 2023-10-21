cb11e57cbc49741334f81a278ded5bf1c2c8f316
ulisesGutierrez <gu362937@uaeh.edu.mx>
2023-10-20 18:16:13 -0600

    constructor

--- javascript/abg_constructor.js
+++ javascript/abg_constructor.js
@@ -1,15 +1,27 @@
-function auto(marca, modelo, anio){
+/*
+Constructor: Funcion que se utiliza para crear objetos.
+Ejemplo:
+*/function auto(marca, modelo, anio){
     this.marca = marca;
     this.modelo = modelo;
     this. anio = anio;
 }
 
 //Construir el objeto:
-
 let nuevoAuto = new auto("Tesla", "Model 3", 2021);
 
 //Para imprimirlo como objeto:
-
-console.log(nuevoAuto);
-
+console.log(nuevoAuto);//auto {marca: "Tesla", modelo: "Model 3", anio: 2021}
 let nuevoAutoDos = new auto("Tesla", "Model X", 2022);
+console.log(nuevoAutoDos);//auto {marca: "Tesla", modelo: "Model X", anio: 2022}
+
+//Otro ejemplo:
+function persona(nombre, apellido, edad){
+    this.nombre = nombre;
+    this.apellido = apellido;
+    this.edad = edad;
+}
+let personaUno = new persona("Juan", "Perez", 30);
+console.log(personaUno);//persona {nombre: "Juan", apellido: "Perez", edad: 30}
+let personaDos = new persona("Maria", "Gomez", 25);
+console.log(personaDos);//persona {nombre: "Maria", apellido: "Gomez", edad: 25}
