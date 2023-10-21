3422dcaa5fe510c5185fb8ec6a8b35689616e8f6
ulisesGutierrez <gu362937@uaeh.edu.mx>
2023-10-20 18:12:44 -0600

    poo

--- javascript/aat_spreadOperator.js
+++ javascript/aat_spreadOperator.js
@@ -11,13 +11,13 @@ a pasar como normalmente la estamos asignando.
 const arr1 = [1 ,2, 3, 4, 5];
 const arr2 = [6, 7, 8, 9, 0];
 
-console.log(arr1, arr2);
+console.log(arr1, arr2);//[1 ,2, 3, 4, 5] [6, 7, 8, 9, 0]
 
 /*
 crear arreglo a partir de los anteriores.
 Esto no es lo correcto:
 const arr3 = [arr1, arr2];
-console.log(arr3);
+console.log(arr3);//[[1 ,2, 3, 4, 5], [6, 7, 8, 9, 0]]
 Con esto ahora si solo tenemos un unico arreglo:
 */
 
@@ -44,9 +44,75 @@ console.log(data);//{id: 1, name: 'luis', age: 29, country: 'MX'}
 //Otro ejemplo:
 
 function listadoFrutasDos (fruta1, fruta2, ...restoDeFrutas) {
-    console.log("fruta 1: ", frruta1);
-    console.log("fruta 2: ", frruta2);
+    console.log("fruta 1: ", frruta1);//Naranja
+    console.log("fruta 2: ", frruta2);//Manzana
 }
 
 let frutas = ["Naranja", "Manzana"];//fruta 1 y fruta 2
 listadoFrutasDos(...frutas, "Sandia", "Pera", "Melón", "Coco");
+
+/*
+Pasar informacion por valor y por referencia:
+Cuando trabajamos con primitivos cualquier tipo de asignacion como la siguiente,
+o cuando lo mandamos a una funcion como un argumento lo estamos mandando por valor
+(primitivos se pasan por valor): 
+*/
+let a = 10;
+let b = a;
+a = 30;
+
+console.log({a, b});//a: 30 b: 10
+
+/*
+Paso por referencia
+(objetos se pasan por referencia):
+*/
+let juan = {nombre: "juan"};
+let ana = juan;//ana, depues de escribir la siguiente linea de codigo
+ana.nombre = "ana";
+
+console.log({juan, ana});//juan: {nombre: "ana"} ana: {nombre: "ana"}
+
+/*Otro caso:
+Cuando se llama a la función cambiarNombre y se le pasa el objeto peter, en realidad se
+está pasando una referencia al objeto peter. Dentro de la función, se modifica la propiedad
+nombre del objeto persona asignándole el valor "Tony". Dado que tanto peter como persona
+hacen referencia al mismo objeto en memoria, cualquier cambio que se realice dentro de la
+función se reflejará en el objeto original.
+
+Por lo tanto, después de llamar a la función cambiarNombre, la propiedad nombre del objeto
+peter se actualiza a "Tony". Luego, la variable tony se asigna al objeto persona devuelto
+por la función, que también es una referencia al mismo objeto modificado. Es por eso que
+cuando se imprime peter y tony, ambos objetos tienen la propiedad nombre igual a "Tony".
+
+En resumen, en JavaScript, los objetos se pasan por referencia, lo que significa que
+cualquier modificación realizada en un objeto dentro de una función afectará al objeto
+original fuera de la función.
+
+const cambiarNombre = (persona) => {
+    persona.nombre = "Tony";
+    return persona;
+}
+
+let peter = {nombre: "Peter"};
+let tony = cambiarNombre(peter);
+
+console.log(peter, tony);//Ambos imprime el nombre de Tony
+
+Vamos a solucionarlo con el operador spread:
+*/
+const cambiarNombre = (...persona) => {
+    persona.nombre = "Tony";
+    return persona;
+}
+
+let peter = {nombre: "Peter"};
+let tony = cambiarNombre(peter);
+
+//Spread operator con arreglos:
+
+const autos = ["bmw", "nissan", "toyota"];
+const otrosAutos = [...autos];
+
+otrosAutos.push("audi");
+console.table({autos, otrosAutos});//autos: (3) ["bmw", "nissan", "toyota"] otrosAutos: (4) ["bmw", "nissan", "toyota", "audi"]
