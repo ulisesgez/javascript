44116727b5e76cd4dd6f723b889ae96bff95d2a4
ulisesGutierrez <gu362937@uaeh.edu.mx>
2023-10-25 12:16:38 -0600

    encapsulamiento

---
+++ javascript/aba_encapsulamiento.js
@@ -0,0 +1,123 @@
+/*
+Encapsulamiento:
+El encapsulamiento es el proceso de agrupar los datos y su comportamiento en una
+misma entidad, evitando el acceso externo a dichos datos.
+Ejemplo:
+*/
+class Course {
+    constructor({
+      name,
+      classes = [],
+    }) {
+      this._name = name;
+      this.classes = classes;
+    }
+  
+    get name() {
+      return this._name;
+    }
+  
+    set name(nuevoNombrecito) {
+      if (nuevoNombrecito === "Curso Malito de Programación Básica") {
+        console.error("Web... no");
+      } else {
+        this._name = nuevoNombrecito; 
+      }
+    }
+  }
+  
+  const cursoProgBasica = new Course({
+    name: "Curso Gratis de Programación Básica",
+  });
+  
+  
+  const cursoDefinitivoHTML = new Course({
+    name: "Curso Definitivo de HTML y CSS",
+  });
+  const cursoPracticoHTML = new Course({
+    name: "Curso Practico de HTML y CSS",
+  });
+  
+  
+  class LearningPath {
+    constructor({
+      name,
+      courses = [],
+    }) {
+      this.name = name;
+      this.courses = courses;
+    }
+  }
+  
+  const escuelaWeb = new LearningPath({
+    name: "Escuela de Desarrollo Web",
+    courses: [
+      cursoProgBasica,
+      cursoDefinitivoHTML,
+      cursoPracticoHTML,
+    ],
+  });
+  
+  const escuelaData = new LearningPath({
+    name: "Escuela de Data Science",
+    courses: [
+      cursoProgBasica,
+      "Curso DataBusiness",
+      "Curso Dataviz",
+    ],
+  });
+  
+  const escuelaVgs = new LearningPath({
+    name: "Escuela de Vidweojuegos",
+    courses: [
+      cursoProgBasica,
+      "Curso de Unity",
+      "Curso de Unreal",
+    ],
+  })
+  
+  class Student {
+    constructor({
+      name,
+      email,
+      username,
+      twitter = undefined,
+      instagram = undefined,
+      facebook = undefined,
+      approvedCourses = [],
+      learningPaths = [],
+    }) {
+      this.name = name;
+      this.email = email;
+      this.username = username;
+      this.socialMedia = {
+        twitter,
+        instagram,
+        facebook,
+      };
+      this.approvedCourses = approvedCourses;
+      this.learningPaths = learningPaths;
+    }
+  }
+  
+  const juan2 = new Student({
+    name: "JuanDC",
+    username: "juandc",
+    email: "juanito@juanito.com",
+    twitter: "fjuandc",
+    learningPaths: [
+      escuelaWeb,
+      escuelaVgs,
+    ],
+  });
+  
+  const miguelito2 = new Student({
+    name: "Miguelito",
+    username: "migelitofeliz",
+    email: "miguelito@juanito.com",
+    instagram: "migelito_feliz",
+    learningPaths: [
+      escuelaWeb,
+      escuelaData,
+    ],
+  });
