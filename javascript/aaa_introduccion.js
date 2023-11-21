c16b7ca38f0c2212f560b71f3246e6a07d3dce1b
ulisesGez <gu362937@uaeh.edu.mx>
2023-11-20 19:44:58 -0600

    Ecma

--- javascript/aaa_introduccion.js
+++ javascript/aaa_introduccion.js
@@ -1,4 +1,5 @@
 /*
+Javascript:
 JavaScript es un lenguaje de programación ampliamente utilizado en el desarrollo web.
 Creado originalmente en 1995, se ha convertido en un pilar fundamental de la
 programación en el lado del cliente en la web. Es un lenguaje versátil y dinámico que
@@ -13,4 +14,21 @@ desarrollo de aplicaciones móviles, servidores (a través de Node.js), y en una
 variedad de otros entornos. Su sintaxis es relativamente sencilla, lo que facilita su
 aprendizaje, y cuenta con una amplia comunidad de desarrolladores que contribuyen a su
 evolución constante.
+
+ECMAScript:
+ECMAScript es un estándar de programación para lenguajes de scripting. Fue creado por la organización ECMA International y es el estándar subyacente para JavaScript, el lenguaje de programación más utilizado en el desarrollo web.
+
+ECMAScript tiene como objetivo lo siguiente:
+
+Define la sintaxis y las funciones básicas de JavaScript
+Objetos y funciones de alto nivel
+Establecer un conjunto de reglas para la creación de scripts y todo el código que puedas crear con JavaScript.
+Crear reglas para la sintaxis básica, funciones, objetos y métodos disponibles en las estructuras de datos del lenguaje.
+Cada versión de ECMAScript (conocida como una “edición”) agrega nuevas características y mejoras al lenguaje. Características como mejoras en la manipulación de objetos, mejoras en las funciones de programación asíncrona y mejoras en el rendimiento del lenguaje.
+
+ECMAScript es ampliamente compatible con los navegadores web y se utiliza en una variedad de entornos, desde el desarrollo web hasta la programación de servidores y aplicaciones móviles. Además, existen varios entornos de ejecución de JavaScript fuera de los navegadores web, como Node.js, que utilizan la especificación ECMAScript.
+
+En resumen, ECMAScript es el estándar subyacente para JavaScript y define las reglas y las características básicas del lenguaje. Cada versión de ECMAScript agrega nuevas características y mejoras al lenguaje, y es ampliamente compatible con los navegadores web y otros entornos de ejecución.
+
+
 */
