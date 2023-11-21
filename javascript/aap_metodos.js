ed0ad85cc970f0c7f0b911dc595f0c2f1dd5b1b4
ulisesGez <gu362937@uaeh.edu.mx>
2023-11-21 12:09:25 -0600

    structure

--- javascript/aap_metodos.js
+++ javascript/aap_metodos.js
@@ -1,3 +1,190 @@
+//Metodos que nos van a permitir transformar textos:
+
+var numero = 444;
+var texto1 = "bienvenido";
+var texto2 = "adios";
+
+var dato = numero.toString();//cambiamos el tipo de dato
+dato = texto1.toUpperCase();//cambiamos el texto a mayusculas
+dato = texto2.toLocaleLowerCase();//cambiamos el texto a minusculas
+console.log(dato);
+
+//Objeto console y metodos:
+
+console.log(console);//Console Object
+console.log(window);//Window Object
+console.dir(window);//DOM
+console.log(global);
+console.log(globalThis);
+console.log(document);//Window Object
+console.dir(document);
+
+console.log("Soy un mensaje");//Soy un mensaje
+console.error("Esto es un error");//Esto es un error
+console.warn("Esto es un aviso");//Esto es un aviso
+console.info("Mensaje informativo");//Mensaje informativo
+
+let miNombre = "name"
+let miApellido = "last name"
+let miEdad = 20
+
+console.log(miNombre , miApellido, miEdad);//name last name 20
+
+//Usando comodines:
+
+console.log(`hola mi miNombre es % %s y tengo %d años`, miNombre , miApellido, miEdad);
+
+console.clear();//limpiar todo de la consola
+
+console.group("lista de compras:");
+console.log("producto 1");
+console.log("producto 2");
+console.log("producto 3");
+console.log("producto 4");
+console.log("producto 5");
+console.log("producto 6");
+console.log("producto 7");
+console.groupEnd();//termina
+
+console.groupCollapsed("lista de compras:");
+console.log("producto 1");
+console.log("producto 2");
+console.log("producto 3");
+console.log("producto 4");
+console.log("producto 5");
+console.log("producto 6");
+console.log("producto 7");
+console.groupEnd();//termina
+
+console.table(Object.entries(console));//Genenrar fila con entries por cada una de las propiedades del objeto console.
+console.table(Object.entries(console).sort);//Ordenar de forma ascendente
+
+//Otros ejemplos con table
+
+const numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+const vocales = ["a", "e", "i", "o", "u"];
+const carro = {
+    marca: "tesla",
+    año: 2022
+};
+
+console.table(numeros);
+console.table(vocales);
+console.table(carro);
+
+//console.timeEnd("Cuanto tiempo tarda mi codigo") aqui vemos un uso de console time
+//console.count("Cuantas veces intento mi codigo")
+
+//Para hacer testing, esto no imprime nada por que se cumple:
+
+let x = 1,//Si lo remplazamos por ejemplo con un 3 marca error e imprime el mensaje
+    y = 2,
+    testXY = "Se espera que X siempre sea menor que Y";
+
+console.assert(x < y, {x, y, testXY});
+
+console.log(Math);//Imprime los metodos disponibles del objeto
+console.log(Math.PI);//3.1416
+console.log(Math.abs(7.8));//Valor absoluto de un numero
+console.log(Math.abs(-7.8));//7.8
+console.log(Math.ceil(7.8));//Redondea hacia arriba 8
+console.log(Math.floor(7.8));//Redondea hacia abajo 7
+console.log(Math.round(7.49));//7
+console.log(Math.sqrt(81));//Raiz cuadrada 9
+console.log(Math.pow(2,5));//Potencia, 2 a la quinta = 32
+console.log(Math.sign(-7.8));//Positivo, negativo, cero -1 0 1
+console.log(Math.random());//valor aleatorio entre 0 y 1
+console.log(Math.random()*1000);//valor aleatorio entre 0 y 1000
+
+console.log(Date);//Imprime varios datos, entre elos la fecha, hora, region
+
+let fecha = new Date();
+console.log(fecha.getDate());//Imprime dia del mes.
+console.log(fecha.getDay());//Numero del dia de la semana, domingo -> 0.
+console.log(fecha.getMonth());//NUmero del mes
+console.log(fecha.getFullYear());//Año actual
+console.log(fecha.getHours());//Hora de 0 a 24
+console.log(fecha.getMinutes());// Minutos
+console.log(fecha.getSeconds());// Segundos
+console.log(fecha.getMilliseconds());// Milisegundos
+
+//Formas mas humanas:
+
+console.log(fecha.toString());//Imprime la fecha en formato string
+console.log(fecha.toDateString());//Imprime la fecha en formato string
+console.log(fecha.toLocaleString());//Imprime la fecha en formato string
+console.log(fecha.toLocaleDateString());//Imprime la fecha en formato string
+console.log(fecha.toLocaleTimeString());//Imprime la fecha en formato string
+console.log(fecha.getTimezoneOffset());//Imprime la fecha en formato string
+
+//Metodos para busqueda:
+let textoUno = "uno, dos, tres, cuatro, cinco, seis, siete, ocho";
+let textoDos = "uno, dos, tres, cuatro, cinco, seis, tres, ocho";
+
+let busquedaUno = textoUno.indexOf("seis");//marca la posicion en la que se encuentra
+console.log(busquedaUno);
+
+let busquedaDos = textoDos.lastIndexOf("tres");//marca la posicion en la que se encuentra el ultimo
+console.log(textoDos);
+
+let busquedaTres = textoDos.search("tress");//manda -1 si es que no le encuentra
+console.log(busquedaTres);
+
+let busquedaCuatro = textoUno.match(/tres/g);//devuelve array de los resultados que encuentra
+console.log(busquedaCuatro);
+
+let busquedaCinco = textoUno.substring(5, 10);//devuelve cadena de texto a partir de un rango
+console.log(busquedaCinco);
+
+let busquedaSeis = textoUno.charAt(5);//devuelve caracter partir de un rango
+console.log(busquedaSeis);
+
+let busquedaSiete = textoDos.includes("cinco");//devuelve true o false si encuentra la busqueda
+console.log(busquedaSiete);
+
+//Metodos para reemplazar:
+let texto = "uno, dos, tres, cuatro, cinco, seis, siete, ocho, noSoyNumber";
+let textoTrim = "    uno, dos, tres, cuatro   ";
+
+let reemplazoUno = texto.replace("noSoyNumber", "nueve");//reemplaza primer valor por el segundo
+console.log(reemplazoUno);
+
+let reemplazoDos = texto.slice(14, 22);//Recorta string
+console.log(reemplazoDos);
+
+let reemplazoTres = texto.split();//Lo mete a un array
+console.log(reemplazoTres);
+
+let reemplazoCuatro = texto.split(" ");//Un array separado
+console.log(reemplazoCuatro);
+
+let reemplazoCinco = textoTrim.trim();//quita espacios por delante y por detras
+console.log(reemplazoCinco);
+
+//Metodos para separar:
+let textoSplit = "uno, dos, tres, cuatro, cinco, seis, siete, ocho";
+
+let separarUno = textoSplit.split(",");//Lo mete a un array
+
+console.log(separarUno);
+
+//Metodos para unir:
+let textoJoin = ["uno", "dos", "tres", "cuatro", "cinco", "seis", "siete", "ocho"];
+
+let unirUno = textoJoin.join();//Lo mete a un array
+
+console.log(unirUno);
+
+//Metodos para cortar:
+let textoSlice = "uno, dos, tres, cuatro, cinco, seis, siete, ocho";
+
+let cortarUno = textoSlice.slice(5);//Lo mete a un array
+
+console.log(cortarUno);
+
+//Metodos en arrays y objetos:
+
+
 //Recorridos con arrays:
 
 let products = [
