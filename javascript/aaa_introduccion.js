77ea8f4104b2afaffd7a2ba95600169c9545092b
ulisesGez <gu362937@uaeh.edu.mx>
2023-11-20 19:58:59 -0600

    intro

--- javascript/aaa_introduccion.js
+++ javascript/aaa_introduccion.js
@@ -30,5 +30,17 @@ ECMAScript es ampliamente compatible con los navegadores web y se utiliza en una
 
 En resumen, ECMAScript es el estándar subyacente para JavaScript y define las reglas y las características básicas del lenguaje. Cada versión de ECMAScript agrega nuevas características y mejoras al lenguaje, y es ampliamente compatible con los navegadores web y otros entornos de ejecución.
 
+TC39:
+TC39 es el comité técnico de ECMAScript, el estándar de javascript. Este comité está compuesto por expertos en el lenguaje y es responsable de su evolución y mantenimiento.
 
+La labor de TC39 se divide en varias etapas, las cuales son las siguientes:
+
+Stage 0: Strawman. En esta etapa, cualquier miembro de TC39 puede proponer una idea o mejora para javascript.
+Stage 1: Proposal. En esta etapa, la idea o mejora propuesta es evaluada y discutida por el comité para determinar si es viable y si se continuará trabajando en ella.
+Stage 2: Draft. En esta etapa, se comienza a trabajar en la especificación formal de la propuesta.
+Stage 3: Candidate. En esta etapa, se considera que la propuesta está lo suficientemente madura como para ser implementada en los navegadores y se espera que sea incluida en la próxima versión del estándar.
+Stage 4: Finished. En esta etapa, la propuesta ha sido aceptada y se incluye en la última versión del estándar.
+Además de las etapas mencionadas, TC39 también se encarga de mantener la compatibilidad entre versiones, de manera que las nuevas características no rompan el código existente.
+
+TC39 es el encargado de asegurar que javascript siga siendo un lenguaje de programación moderno y relevante. Los miembros de TC39 son expertos en javascript y sus decisiones afectan directamente a la forma en que se desarrolla el lenguaje y a las características que estarán disponibles en el futuro.
 */
