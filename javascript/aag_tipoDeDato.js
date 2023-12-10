45f5a6353d33c74e41087dae644d08375a6313f1
ulisesGez <gu362937@uaeh.edu.mx>
2023-12-10 13:22:53 -0600

    datatypes

--- javascript/aag_tipoDeDato.js
+++ javascript/aag_tipoDeDato.js
@@ -424,3 +424,45 @@ wm.set(llave1, 1);
 wm.set(llave2, 2);
 
 console.log(wm);
+
+console.log(wm.has(llave1));//true
+console.log(wm.has(llave3));//false
+
+console.log(wm.get(llave1));//1
+console.log(wm.get(llave3));//undefined
+
+wm.delete(llave1);
+console.log(wm.has(llave1));//false
+
+/*
+Iterables e Iterators:
+Iterables: Es un tipo de dato que puede ser recorrido.
+Iterators: Es un objeto que contiene un metodo next() que devuelve un objeto con dos propiedades:
+value: es el valor del elemento del iterable.
+done: es un boolean que nos indica si el iterable tiene mas elementos para recorrer.
+Cuando hablamos de un tipo de dato iterable, significa que es una estructura de datos lineal,
+que hace que sus elementos sean publicos y se puedan recorrer por ejemplo, arreglos, strings, maps, sets.
+*/
+const iterable = [1, 2, 3, 4, 5];//iterable es el arreglo
+const iterableString = "hola" 
+//Accedemos al iterador del iterable:
+const iterador = iterable[Symbol.iterator]();
+
+console.log(iterable);//(5) [1, 2, 3, 4, 5]
+console.log(iterador);//ArrayIterator
+console.log(iterador.next());//{ value: 1, done: false }
+console.log(iterador.next());//{ value: 2, done: false }
+console.log(iterador.next());//{ value: 3, done: false }
+console.log(iterador.next());//{ value: 4, done: false }
+console.log(iterador.next());//{ value: 5, done: false }
+console.log(iterador.next());//{ value: undefined, done: true }
+
+//Optimizar k,o anterior:
+
+let next = iterador.next();
+while(!next.done) {
+    console.log(next.value);
+    next = iterador.next();
+}
+
+console.log(iterableString);//StringIterator
