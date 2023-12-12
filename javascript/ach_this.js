4f4ca6b29318e475f18bc8ff1d139d754bfaf688
ulisesGez <gu362937@uaeh.edu.mx>
2023-12-11 19:15:37 -0600

    this

--- javascript/ach_this.js
+++ javascript/ach_this.js
@@ -1,5 +1,5 @@
 /*
-This:
+This en el contexto global:
 This es un objeto que hace referencia al contexto de ejecucion, es decir,
 al objeto que contiene el codigo que se esta ejecutando en este momento.
 En el contexto global, this hace referencia al objeto global, ya sea window
@@ -17,7 +17,6 @@ this.nombre = "Contexto global";//Al ver las propiedades en la consola nombre = 
 console.log(this.nombre);//Contexto global
 
 //En una funcion:
-
 function imprimir() {
     console.log(this.nombre);
 }
@@ -31,15 +30,16 @@ const obj = {
     }
 }
 
-obj.imprimir();//Contexto objeto
+obj.imprimir();//Contexto objeto, no imprime Contexto global como algunos crerian
 
 const objDos = {
     nombre: 'Contexto objetoDos',
     imprimir//hace referencia a function imprimir() { console.log(this.nombre); }
 }
 
-objDos.imprimir();//Contexto objetoDos, no imprime Contexto global como algunos crerian
+objDos.imprimir();//Contexto objetoDos, no imprime Contexto global como algunos crerian ya que this hace referencia al objeto que contiene la funcion, es decir, al objeto que esta ejecutando la funcion.
 
+//this en el contexto de una arrow function:
 const objTres = {
     nombre: 'Contexto objetoTres',
     //Las arrow function no manejan su propio scope:
@@ -50,38 +50,10 @@ const objTres = {
 
 objTres.imprimir();//Contexto global
 
-//En programacion orientada a objetos:
-
 function Persona(nombre) {
     //funcion constructura que crea su propio scope:
     this.nombre = nombre;
-    return console.log(this.nombre);//ulises
+    //return console.log(this.nombre);//ulises        1)
 }
 
-let ulises = new Persona('ulises')
-
-function Carro(marca) {
-    //funcion constructura que crea su propio scope:
-    this.marca = marca;
-    //Crea un nuevo scope o contexto, internamente no tiene una propiedad marca:
-    return function() {
-        console.log(this.marca);//ya no imprime ford, en este caso es udefined
-    }
-}
-
-let ford = new Carro('ford')
-ford();
-/*
-Imprimiria Contexto global si en lugar de marca fuera nombre y la clase fuera Persona en lugar de carro.
-Imprime undefined.
-
-Para solucionar esto, y que nos imprima ford,las arrow function no generan scope interno:
-
-function Carro(marca) {
-    this.marca = marca;
-    return () => console.log(this.marca);//ford
-}
-
-let ford = new Carro('ford')
-ford();
-*/
+let ulises = new Persona('ulises');//ulises
