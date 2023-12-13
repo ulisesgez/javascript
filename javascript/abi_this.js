2149aed6c05f64da4e09d568302e7cb61abd3410
ulisesGez <gu362937@uaeh.edu.mx>
2023-12-13 15:18:38 -0600

    this

--- javascript/abi_this.js
+++ javascript/abi_this.js
@@ -68,3 +68,129 @@ function Persona(nombre) {
 //let ulises = new Persona('ulises');//ulises --> caso 1
 //ulises();//Contexto global, ya que this hace referencia al objeto global, es decir, window en el navegador o global en nodejs  --> caso 2
 ulises();//ulises --> caso 3, recuerda las arrow function no manejan su propio scope.
+
+/*
+Call, Apply y Bind:
+Call y Apply son metodos que nos permiten invocar una funcion y establecer el valor
+de this, el primer parametro de call y apply es el valor que queremos que tenga this
+cuando se ejecute la funcion, la diferencia entre call y apply es que call recibe los
+parametros de la funcion de forma separada y apply los recibe en un arreglo.
+
+Bind es un metodo que nos permite crear funciones enlazadas a un contexto en particular,
+en otras palabras, bind nos permite crear funciones que cuando sean invocadas tendran
+el valor de this establecido como nosotros queramos.
+
+Son metodos que nos permiten conservar la referencia de un scope en particular y utilizarlo
+por ejemplo en alguna invocacion o en algun codigo para eso nos sirve call apply y bind.
+*/
+
+console.log(this);//objeto window(contexto global) en javascript de los navegadores
+console.log(this);//objeto global en nodejs
+console.log(this === window);//true
+
+this.lugar = "Contexto Global"
+
+function saludar() {
+    console.log(`${this.lugar}`);
+}
+
+function depedir(despedida, aQuien) {
+    console.log(`${despedida} ${aQuien} desde el ${this.lugar}`);
+}
+
+saludar();//Contexto Global
+depedir("bye", "ulises");//bye ulises desde el Contexto Objeto
+
+const objCuatro = {
+    lugar:"Contexto Objeto"
+}
+
+saludar.call(objCuatro);//En lugar de imprimir Contexto Global, imprime Contexto Objeto
+saludar.apply(objCuatro);//En lugar de imprimir Contexto Global, imprime Contexto Objeto
+
+depedir.call(objCuatro, "adios", "ulises");//adios ulises desde el Contexto Objeto
+depedir.call(null, "adios", "ulises");//adios ulises desde el Contexto Global
+depedir.call(this, "adios", "ulises");//adios ulises desde el Contexto Global
+depedir.apply(objCuatro, ["nos vemos", "ulises"]);//nos vemos ulises desde el Contexto Objeto
+depedir.apply(null, ["nos vemos", "ulises"]);//nos vemos ulises desde el Contexto Global
+depedir.apply(this, ["nos vemos", "ulises"]);//nos vemos ulises desde el Contexto Global
+
+//Ahora con bind:
+
+const persona = {
+    nombre: 'ulises',
+    saludar: function() {
+        console.log(`HOLA ${this.nombre}`);
+    }
+}
+
+persona.saludar();//HOLA ulises
+
+/*
+const persona = {
+    nombre: 'ulises',
+    saludar: ()=>{
+        console.log(`HOLA ${this.nombre}`);
+    }
+}
+
+persona.saludar();//HOLA undefined
+*/
+const otraPersona = {
+  //saludar: persona.saludar//HOLA undefined
+  saludar: persona.saludar.bind(persona)//HOLA ulises
+}
+
+otraPersona.saludar();
+
+this.nombre = 'window';
+
+const otraPersonaMas = {
+  saludar: persona.saludar.bind(this)//HOLA window
+}
+
+otraPersonaMas.saludar();
+
+//Otro ejemplo con Call y paso de parametros:
+
+let mouseOne = {
+    marca: 'razer',
+    modelo: '21212',
+    color:'black',
+    caracteristicas: function(microprocesador) {//no lleva this al pasarse como parametro:
+      return this.marca + ', ' + this.modelo + ', ' + this.color + microprocesador;
+    }
+  }
+  
+  let mouseTwo = {
+    marca: 'logitech',
+    modelo: '01010',
+    color:'green',
+  }
+  
+  console.log(mouseOne.caracteristicas('arm'));
+  //Ahora queremos ver las caracteristicas de mouseTwo:
+  console.log(mouseOne.caracteristicas.call(mouseTwo, 'arm'));
+  
+  //Apply, ocurre lo mismo que en call:
+  
+  let tvOne = {
+    marca: 'samsung',
+    modelo: '21212',
+    color:'black',
+    caracteristicas: function(pulgadas) {//no lleva this al pasarse como parametro:
+      return this.marca + ', ' + this.modelo + ', ' + this.color + pulgadas;
+    }
+  }
+  
+  let tvTwo = {
+    marca: 'lg',
+    modelo: '01010',
+    color:'green',
+  }
+  
+  console.log(tvOne.caracteristicas(50));
+  //Ahora queremos ver las caracteristicas de tvTwo, en cuestion de parametros es diferente::
+  let arreglo = [40];
+  console.log(tvOne.caracteristicas.apply(tvTwo, arreglo));
+  //console.log(tvOne.caracteristicas.apply(tvTwo, [40]));//tambien se ppuede pasar asi
