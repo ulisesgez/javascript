9b35a0615f9b0b1e2a53073fe3a0d0f583955b1d
ulisesGez <gu362937@uaeh.edu.mx>
2023-12-20 18:07:10 -0600

    parametros

--- javascript/adm_ parametros.js
+++ javascript/adm_ parametros.js
@@ -4,88 +4,70 @@ Para pasar parametros a una funcion manejadora de eventos, se puede hacer de dos
 1.- Usando una funcion anonima
 2.- Usando una arrow function
 
-Para remover un evento, se usa el metodo removeEventListener, el cual recibe dos parametros:
+Para remover un evento, se usa el metodo removeEventListener, el cual recibe
+dos parametros:
 1.- El tipo de evento
 2.- La funcion manejadora del evento
 
-Para desabilitar un boton, se usa la propiedad disabled, la cual recibe un valor booleano:
+Para desabilitar un boton, se usa la propiedad disabled, la cual recibe un
+valor booleano:
 true: desabilita el boton
 false: habilita el boton
 
-Para ver los eventos que se pueden usar en un elemento, se puede usar el metodo getEventListeners, el cual recibe como parametro el elemento del cual se quiere ver los eventos.
+Para ver los eventos que se pueden usar en un elemento, se puede usar el
+metodo getEventListeners, el cual recibe como parametro el elemento del cual se quiere ver los eventos.
 */
-
-function holaMundo() {
-    alert('hola mundo');
-    console.log(event);//PointerEvent {isTrusted: true, pointerId: 1, width: 1, height: 1, pressure: 0, …}
-}
+const $btnParametros = document.getElementById("evento-parametros");
+const $btnRemover = document.getElementById("evento-remover");
 
 function saludar(nombre = "desconocido") {
     alert(`hola ${nombre}`);
 }
-
-const $eventoSemantico = document.getElementById("evento-semantico");
-const $eventoMultiple = document.getElementById("evento-multiple");//con esto podemos ejecutar varias funciones en un evento
-const $eventoRemover = document.getElementById("evento-remover");
-
-$eventoSemantico.onclick = holaMundo;//Sin parentesis, ya que se ejecutara cuando entremos a la pagina.
-//Ya no se ejecutara la funcion anterior, se reemplaza, veamos en accion los eventos multiples:
-$eventoSemantico.onclick = function(e) {
-    alert('nuevo manejador de eventos semantico');
-    console.log(e);//event, pasado como parametro, suele escribirse solo la e
-    //PointerEvent {isTrusted: true, pointerId: 1, width: 1, height: 1, pressure: 0, …}
-    console.log(event);//event
-    //PointerEvent {isTrusted: true, pointerId: 1, width: 1, height: 1, pressure: 0, …}
-}
-
-$eventoMultiple.addEventListener("click", holaMundo);
-$eventoMultiple.addEventListener("click", (e) => {
-    alert("multiple");
-    console.log(e);//event
-    console.log(e.type);//tipo de eevento, click
-    console.log(e.target);//objeto que origina el evento, <button>...<button>
-});
-
 /*
-hola [object PointerEvent], no manda a traer el parametro predefinido de desconocido:
+$btnParametros.addEventListener("click", saludar);//hola [object PointerEvent], no imprime "hola desconocido" como pudieramos pensar.
 
-function saludar(nombre = "desconocido") {
-    alert(`hola ${nombre}`);
-}
-
-$eventoMultiple.addEventListener("click", saludar);
-
-Tampoco lo solucionamos:
-
-function saludar(nombre = "desconocido") {
-    alert(`hola ${nombre} - ${event}`);
-}
-//Imprime: hola [object PointerEvent] - [object PointerEvent]
-$eventoMultiple.addEventListener("click", saludar);
-
-Solucion antes:
-
-//Imprime: hola desconocido:
-
-$eventoMultiple.addEventListener("click", function () {
-    saludar();
-});
-
-//Solucion nueva usando arrow functions:
-//Aqui si ponemos el parentesis, por que la funcion manejadora es la arrow function
-$eventoMultiple.addEventListener("click", () => saludar());
+Para solucionar esto, se puede usar una funcion anonima o una arrow function:
+1.- Usando una funcion anonima:
 */
-$eventoMultiple.addEventListener("click", () => {
-    saludar();//hola desconocido
-    saludar("jesus");//hola jesus
-});
 
-const removerDobleClick = (e) => {
-    alert(`removiendo el evento del tipo ${e.type}`);
-    console.log(e);
-    $eventoRemover.removeEventListener("dblclick", removerDobleClick);
-    $eventoRemover.disabled = true;//Con esto desabilitamos el botos despues de presionar dos veces
-
+$btnParametros.addEventListener("click", function() {
+    saludar("ulises");
 }
-//solo funcionara 1 vez despues de dar doble click sobre el boton, despues de ello ya no hara nada.
-$eventoRemover.addEventListener("dblclick", removerDobleClick);
+);
+//2.- Usando una arrow function:
+$btnParametros.addEventListener("click", () => {
+    saludar("jose");
+}
+);
+/*
+Remover eventos:
+El siguiente ejemplo remueve el evento saludar, al dar click en el boton remover eventos.
+
+$btnRemover.addEventListener("click", () => {
+    $btnParametros.removeEventListener("click", saludar);//No funciona, no remueve el evento.
+}
+);
+
+//Para que funcione, se debe usar una funcion anonima:
+const saludarUlises = function() {
+    saludar("Ulises");
+};
+$btnParametros.addEventListener("click", saludarUlises);
+
+const saludarJose = () => {
+    saludar("Jose");
+};
+$btnParametros.addEventListener("click", saludarJose);
+
+$btnRemover.addEventListener("click", () => {
+    $btnParametros.removeEventListener("click", saludarUlises);
+    $btnParametros.removeEventListener("click", saludarJose);
+});
+Remover eventos:
+*/
+const removerClick = () => {
+    alert("remover click, solo se puede remover una vez");
+    $btnRemover.removeEventListener("click", removerClick);
+    $btnRemover.disabled = true;//Desabilita el boton remover eventos una vez que se removio el evento.
+}
+$btnRemover.addEventListener("click", removerClick);//Segun la logica del programa solo podemos remover una vez el evento.
