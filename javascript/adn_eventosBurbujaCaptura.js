9cdd67a0f182fec38fb0c59b6dbc00740571d6b3
ulisesGez <gu362937@uaeh.edu.mx>
2023-12-22 16:56:21 -0600

    burbujaCaptura

--- javascript/adn_eventosBurbujaCaptura.js
+++ javascript/adn_eventosBurbujaCaptura.js
@@ -6,30 +6,20 @@ Captura: Se propaga desde el elemento mas externo al mas interno.
 
 const $divEventos = document.querySelectorAll(".eventos-flujo div");
 
+//console.log($divEventos);//NodeList(3) [div.uno, div.dos, div.tres]
+
 function flujoEventos(e) {
-    //console.log(`hola`);
-    console.log(`hola te saluda ${this.className}, el click lo origino ${e.target.className}`);
+    //console.log("Hola");//Imprime 3 veces Hola si se hace click en el div padre, contrario a si comentamos la linea con false y descomentamos en true.
+    console.log(`Hola te saluda ${this.className}, el click lo originó ${e.target.className}`);//Imprime 3 mensajes: Hola te saluda uno/dos/tres, el click lo originó tres.
 }
 
-console.log($divEventos);
-//Crear addEventListener de manera dinamica para los divs(3):
-
-$divEventos.forEach(div => {
-    //Podemos ver que al dar click en los divs esto se propaga en los mensajes que se imprime.
-    //Fase de burbuja, va del elemento mas interno al mas externo:
-    //div.addEventListener("click", flujoEventos, {
-    //    capture: false
-    //});
+//AddEventListener de forma dinaica:
+$divEventos.forEach((div) => {
+    //Fase de burbuja(Por defecto viene en false):
     //div.addEventListener("click", flujoEventos);
-    //div.addEventListener("click", flujoEventos, false);
-    //Captura, va del elemento mas externo al mas interno:
-    //div.addEventListener("click", flujoEventos, {
-    //    capture: true
-    //});
-    //div.addEventListener("click", flujoEventos, {
-    //capture: true,
-    //Con este solo nos deja dar un click:
-    //onnce: true
-    //});
-    div.addEventListener("click", flujoEventos, true);
-})
+    //Fase de burbuja(Especificandolo de forma explicita):
+    div.addEventListener("click", flujoEventos, false);//console.log("Hola");//Imprime 3 veces Hola si se hace click en el div padre, contrario a si comentamos la linea con false y descomentamos en true.
+    //Fase de captura:
+    //div.addEventListener("click", flujoEventos, true);
+}
+);
