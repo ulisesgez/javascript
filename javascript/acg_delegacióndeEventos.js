cde9036e65ad90e98072460c694e40dfeb1964ba
ulisesGez <gu362937@uaeh.edu.mx>
2023-12-23 16:50:28 -0600

    dom

--- javascript/acg_delegacióndeEventos.js
+++ javascript/acg_delegacióndeEventos.js
@@ -1,9 +1,20 @@
+/*
+Delegacion de eventos:
+Es una tecnica que consiste en asignar un evento a un elemento padre y
+a traves de este, determinar el elemento al cual se le disparo el evento.
+
+Ventajas:
+1. Optimizar recursos del navegador
+2. Evitar asignacion de eventos a elementos que aun no existen en el DOM
+3. Evitar bucles de eventos
+*/
+
 function flujoEventos(e) {
     console.log(`hola te saluda ${this}, el click lo origino ${e.target.className}`);
 }
 //Como el document es el nodo raiz de nuestro documento, ya no hay un elemento padre despues de el, no es necesario el manejo de stopPropagation
 document.addEventListener("click", (e) => {
-    console.log("click en ", e.target);//NOs dice en consola a que le estamos dando click
+    console.log("click en ", e.target);//Nos dice en consola a que le estamos dando click
 
     if(e.target.matches(".eventos-flujo div")) {
         flujoEventos(e);
