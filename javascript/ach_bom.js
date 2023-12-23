cb38d93ad7efbb6883225a30f3fe4cd38513ed14
ulisesGez <gu362937@uaeh.edu.mx>
2023-12-23 17:02:46 -0600

    bom

---
+++ javascript/ach_bom.js
@@ -0,0 +1,27 @@
+/*
+Bom:
+El BOM se refiere al Browser Object Model (Modelo de Objetos del Navegador)
+en el contexto de JavaScript y la web. Aunque a veces se confunde con el DOM,
+el BOM es diferente y se centra más en la interacción del navegador en lugar
+de la estructura del documento.
+
+El BOM es una colección de objetos proporcionados por el navegador web que no
+está estandarizada por el W3C (Consorcio World Wide Web), lo que significa que
+puede variar entre diferentes navegadores. Algunos de los objetos y funcionalidades
+comunes que forman parte del BOM son:
+
+Objeto Window: Es el objeto raíz del BOM. Representa la ventana del navegador
+y proporciona métodos para manipularla, abrir nuevas ventanas, mostrar diálogos, etc.
+
+Objeto Navigator: Proporciona información sobre el navegador del usuario,
+como el nombre del navegador, la versión, el lenguaje, etc.
+
+Objeto Screen: Proporciona información sobre la pantalla del dispositivo del usuario,
+como la altura, el ancho, la profundidad del color, etc.
+
+Objeto History: Permite la interacción con el historial del navegador,
+permitiendo avanzar o retroceder entre las páginas visitadas.
+
+Objeto Location: Proporciona información sobre la URL de la página actual y
+permite redireccionar a otras páginas.
+*/
