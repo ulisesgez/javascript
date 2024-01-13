d16cf006bcccadd4dbd605e60113220c5a4189b1
ulisesGez <gu362937@uaeh.edu.mx>
2024-01-13 16:21:32 -0600

    crud

---
+++ home/acu_apiRestCrud.js
@@ -0,0 +1,20 @@
+/*
+Crud:
+Por las siglas en inglés de Create, Read, Update y Delete, es un acrónimo que
+se utiliza para hacer referencia a las funciones básicas en bases de datos o la
+capa de persistencia en un software.
+
+Create: Crear
+Read: Leer
+Update: Actualizar
+Delete: Eliminar
+
+Estas son las cuatro operaciones básicas que se pueden realizar sobre una base
+de datos o una capa de persistencia.
+
+En este caso, se va a realizar un crud sobre una base de datos de MongoDB
+utilizando NodeJS y Express.
+
+Para realizar las operaciones CRUD, se va a utilizar el método HTTP POST, ya
+que este método permite enviar datos a un servidor para crear un recurso.
+*/
