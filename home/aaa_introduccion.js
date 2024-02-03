600349d4221796036a1d1accba5e97671748281d
ulisesGez <gu362937@uaeh.edu.mx>
2024-02-03 16:53:40 -0600

    text

--- home/aaa_introduccion.js
+++ home/aaa_introduccion.js
@@ -17,7 +17,6 @@ evolución constante.
 
 ECMAScript:
 ECMAScript es un estándar de programación para lenguajes de scripting. Fue creado por la organización ECMA International y es el estándar subyacente para JavaScript, el lenguaje de programación más utilizado en el desarrollo web.
-
 ECMAScript tiene como objetivo lo siguiente:
 
 Define la sintaxis y las funciones básicas de JavaScript
@@ -27,11 +26,10 @@ Crear reglas para la sintaxis básica, funciones, objetos y métodos disponibles
 Cada versión de ECMAScript (conocida como una “edición”) agrega nuevas características y mejoras al lenguaje. Características como mejoras en la manipulación de objetos, mejoras en las funciones de programación asíncrona y mejoras en el rendimiento del lenguaje.
 
 ECMAScript es ampliamente compatible con los navegadores web y se utiliza en una variedad de entornos, desde el desarrollo web hasta la programación de servidores y aplicaciones móviles. Además, existen varios entornos de ejecución de JavaScript fuera de los navegadores web, como Node.js, que utilizan la especificación ECMAScript.
-
 En resumen, ECMAScript es el estándar subyacente para JavaScript y define las reglas y las características básicas del lenguaje. Cada versión de ECMAScript agrega nuevas características y mejoras al lenguaje, y es ampliamente compatible con los navegadores web y otros entornos de ejecución.
 
 TC39:
-TC39 es el comité técnico de ECMAScript, el estándar de javascript. Este comité está compuesto por expertos en el lenguaje y es responsable de su evolución y mantenimiento.
+Es el comité técnico de ECMAScript, el estándar de javascript. Este comité está compuesto por expertos en el lenguaje y es responsable de su evolución y mantenimiento.
 
 La labor de TC39 se divide en varias etapas, las cuales son las siguientes:
 
