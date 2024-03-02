e1e65c110dd6a9d72b26ca4080f280be614eff76
ulisesGez <105885240+ulisesgez@users.noreply.github.com>
2024-03-02 15:58:20 -0600

    order

--- home/aad_tipoDeDato.js
+++ home/aad_tipoDeDato.js
@@ -70,6 +70,102 @@ console.log(Number(falseNumber) + 7);//40
 console.log(parseInt(falseNumber) + 7); numero entero, tambien lo convierte.
 console.log(parseFloat(falseNumber) + 7); l0o convierte a decimal.
 
+Booleano:
+Los valores booleanos solo pueden ser uno de dos valores: trueo false.
+Son básicamente pequeños interruptores de encendido y apagado, donde trueestá encendido y
+falseapagado. Estos dos estados son mutuamente excluyentes.
+
+Nota: los valores booleanos nunca se escriben entre comillas.
+Las cadenas "true"y "false"no son booleanas y no tienen un
+significado especial en JavaScript
+
+Truthy y Falsy:
+En JavaScript, un valor booleano se conoce como un valor de verdad o falsedad. Los valores
+que no son estrictamente booleanos también tienen un valor de verdad o falsedad.
+Cuando se evalúan en un contexto booleano, estos valores se consideran verdaderos o falsos.
+Los valores que se consideran falsos se conocen como valores falsos, mientras que los valores
+que se consideran verdaderos se conocen como valores verdaderos.
+
+Tipo de valor verdadero y falso:
+*/
+
+console.log(Boolean(0));//falso
+console.log(Boolean(null));
+console.log(Boolean(NaN));
+console.log(Boolean(undefined));
+console.log(Boolean(false));
+console.log(Boolean(""));
+
+console.log(Boolean(1));//Verdadero
+console.log(Boolean(-7));
+console.log(Boolean("a"));
+console.log(Boolean([]));
+console.log(Boolean({}));
+
+//Usando el constructor:
+
+let v = Boolean(true);
+let f = Boolean(false);
+
+//OR:
+console.log(-7 || "valor de la derecha");//-7
+console.log(true || "valor de la derecha");//-true
+console.log(undefined || "valor de la derecha");//valor de la derecha
+console.log(0 || "valor de la derecha");//valor de la derecha
+
+//AND:
+console.log(-7 && "valor de la derecha");//valor de la derecha
+console.log(true && "valor de la derecha");//valor de la derecha
+console.log(undefined && "valor de la derecha");//undefined
+console.log(0 && "valor de la derecha");//0
+
+//Booleanos y operadores lógicos:
+
+console.log("Negación");
+console.log(!true);//false
+console.log(!false);//true
+
+console.log("And");
+console.log(true && true);//true
+console.log(true && false);//false
+console.log(false && true);//false
+console.log(false && false);//false
+
+console.log("Or");
+console.log(true || true);//true
+console.log(true || false);//true
+console.log(false || true);//true
+console.log(false || false);//false
+
+console.log("Mix");
+console.log(true && (false || true));//true
+console.log(false || (true && false));//false
+console.log((false || true) && (true && true));//true
+
+//Asignaciones:
+
+const soyUndefined = undefined;
+const soyNull = null;
+const soyFalso = false;
+
+const a1 = true && "Hola Mundo" && 150;//150
+const a2 = "Hola" && "Mundo" && soyFalso && true;//false
+const a3 = soyFalso || "Ya no soy falso" || soyUndefined || soyNull;//Ya no soy falso
+const a4 = soyFalso || soyUndefined || "Ya no soy falso" || soyNull;//Ya no soy falso
+const a5 = soyFalso || soyUndefined || soyNull || "Ya no soy falso";//Ya no soy falso
+const a6 = soyFalso || soyUndefined || soyNull || "Ya no soy falso" || true;//true
+const a7 = soyFalso || soyUndefined || soyNull || "Ya no soy falso" || true || false;//true
+const a8 = soyFalso || soyUndefined || soyNull || "Ya no soy falso" || true || false || "Ya no soy falso";//true
+const a9 = soyFalso || soyUndefined || soyNull || "Ya no soy falso" || true || false || "Ya no soy falso" || "Ya no soy falso";//true
+const a10 = soyFalso || soyUndefined || soyNull || "Ya no soy falso" || true || false || "Ya no soy falso" || "Ya no soy falso" || "Ya no soy falso";//true
+const a11 = soyFalso || soyUndefined || soyNull || "Ya no soy falso" || true || false || "Ya no soy falso" || "Ya no soy falso" || "Ya no soy falso" || "Ya no soy falso";//true
+const a12 = soyFalso || "Ya no soy falso" || soyUndefined || soyNull;//Ya no soy falso
+const a13 = soyFalso || "Ya no soy falso";//Ya no soy falso
+
+console.log(a1);
+console.log(a2);
+
+/*
 Undefined, Nan y null:
 
 Tanto null como undefined van a representar un valor ausente, es decir una
