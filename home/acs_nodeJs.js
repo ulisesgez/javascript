21d77395b5c187f7fdf219148083e986c7ce6590
ulisesGez <105885240+ulisesgez@users.noreply.github.com>
2024-03-02 16:53:32 -0600

    node

--- home/acs_nodeJs.js
+++ home/acs_nodeJs.js
@@ -2,4 +2,11 @@
 NodeJs:
 NodeJs es un entorno de ejecución de JavaScript que permite ejecutarlo fuera
 del navegador.Se encuentra onstruido con el motor de JavaScript V8 de Chrome.
+Node.js es un entorno de ejecución de JavaScript de código abierto y multiplataforma que
+permite a los desarrolladores crear aplicaciones de servidor y de red de alto rendimiento.
+Utiliza el motor V8 de Google Chrome para ejecutar código JavaScript fuera del navegador web.
+Node.js es conocido por su eficiencia y escalabilidad, lo que lo hace popular para construir
+aplicaciones web en tiempo real, APIs RESTful, servidores de aplicaciones, microservicios
+y mucho más. Además, Node.js cuenta con un ecosistema de paquetes robusto a través de npm
+(Node Package Manager), que facilita la instalación y gestión de dependencias en proyectos Nodejs.
 */
