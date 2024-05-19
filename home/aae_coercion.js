075bade7f9a8e26ae82c8679ff29965de277929d
ulisesGez <105885240+ulisesgez@users.noreply.github.com>
2024-05-19 07:43:14 -0600

    coercion

--- home/aae_coercion.js
+++ home/aae_coercion.js
@@ -5,26 +5,24 @@ Es el proceso en el cual JavaScript intenta convertir automáticamente un valor 
 tipo a otro, para que puedan ser comparados o operados.
 Esto puede dar lugar a algunos resultados inesperados si no se tiene en cuenta.
 
-Tipos de datos:
-Primitivos: string, number, boolean, null, undefined, NaN.
-Compuestos: object, array, function, class, etc
-Ejemplo:
-*/
-
-4 + "7"; // 47
-4 * "7"; // 28
-2 + true // 3
-false - 3 // -3
-
-/*
-Coerción:
-La forma en la que podemos cambiar un tipo de valor a otro tipo de valor
+La coerción es la forma en la que podemos cambiar un tipo de valor a otro tipo de valor
 (valor tipo numero a valor tipo string).
 Coerción implicita: Es cuando el lenguaje nos ayuda y cambia de un tipo de valor a
 otro tipo de valor 
 Coerción explicita: Es la forma en la que nosotros obligamos a que un valor de
 un tipo cambie a otro valor de otro tipo
 
+Tipos de datos:
+Primitivos: string, number, boolean, null, undefined, NaN.
+Compuestos: object, array, function, class, etc
+Ejemplo:
+*/
+4 + "7"; // 47
+4 * "7"; // 28
+2 + true // 3
+false - 3 // -3
+
+/*
 Coerción implicita:
 */
 
