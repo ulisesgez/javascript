52fade18bfc54a0d9e8c361fce57241dd017e48e
ulisesGez <105885240+ulisesgez@users.noreply.github.com>
2024-09-09 10:24:18 -0600

    space

--- home/aaa_introduccion.js
+++ home/aaa_introduccion.js
@@ -2,7 +2,9 @@
 Javascript:
 JavaScript es un lenguaje de programación ampliamente utilizado en el desarrollo web.
 Creado originalmente en 1995, se ha convertido en un pilar fundamental de la
-programación en el lado del cliente en la web. Es un lenguaje versátil y dinámico que
+programación en el lado del cliente en la web.
+
+Es un lenguaje versátil y dinámico que
 permite a los desarrolladores crear aplicaciones interactivas y dinámicas en el
 navegador. JavaScript se ejecuta en el lado del cliente, lo que significa que
 funciona en el navegador del usuario y permite la manipulación de elementos HTML,
