1bdfd2dfb16a94cf54096830ba757414192e2faa
ulisesGez <105885240+ulisesgez@users.noreply.github.com>
2025-01-13 09:28:55 -0600

    space

--- home/aaa_introduccion.js
+++ home/aaa_introduccion.js
@@ -26,22 +26,34 @@ Define la sintaxis y las funciones básicas de JavaScript
 Objetos y funciones de alto nivel
 Establecer un conjunto de reglas para la creación de scripts y todo el código que puedas crear con JavaScript.
 Crear reglas para la sintaxis básica, funciones, objetos y métodos disponibles en las estructuras de datos del lenguaje.
-Cada versión de ECMAScript (conocida como una “edición”) agrega nuevas características y mejoras al lenguaje. Características como mejoras en la manipulación de objetos, mejoras en las funciones de programación asíncrona y mejoras en el rendimiento del lenguaje.
+Cada versión de ECMAScript (conocida como una “edición”) agrega nuevas características y mejoras al lenguaje.
+Características como mejoras en la manipulación de objetos, mejoras en las funciones de programación asíncrona y
+mejoras en el rendimiento del lenguaje.
 
-ECMAScript es ampliamente compatible con los navegadores web y se utiliza en una variedad de entornos, desde el desarrollo web hasta la programación de servidores y aplicaciones móviles. Además, existen varios entornos de ejecución de JavaScript fuera de los navegadores web, como Node.js, que utilizan la especificación ECMAScript.
-En resumen, ECMAScript es el estándar subyacente para JavaScript y define las reglas y las características básicas del lenguaje. Cada versión de ECMAScript agrega nuevas características y mejoras al lenguaje, y es ampliamente compatible con los navegadores web y otros entornos de ejecución.
+ECMAScript es ampliamente compatible con los navegadores web y se utiliza en una variedad de entornos, desde el
+desarrollo web hasta la programación de servidores y aplicaciones móviles. Además, existen varios entornos de ejecución
+de JavaScript fuera de los navegadores web, como Node.js, que utilizan la especificación ECMAScript.
+En resumen, ECMAScript es el estándar subyacente para JavaScript y define las reglas y las características básicas del
+lenguaje. Cada versión de ECMAScript agrega nuevas características y mejoras al lenguaje, y es ampliamente compatible
+con los navegadores web y otros entornos de ejecución.
 
 TC39:
-Es el comité técnico de ECMAScript, el estándar de javascript. Este comité está compuesto por expertos en el lenguaje y es responsable de su evolución y mantenimiento.
+Es el comité técnico de ECMAScript, el estándar de javascript. Este comité está compuesto por expertos en el lenguaje
+y es responsable de su evolución y mantenimiento.
 
 La labor de TC39 se divide en varias etapas, las cuales son las siguientes:
 
 Stage 0: Strawman. En esta etapa, cualquier miembro de TC39 puede proponer una idea o mejora para javascript.
-Stage 1: Proposal. En esta etapa, la idea o mejora propuesta es evaluada y discutida por el comité para determinar si es viable y si se continuará trabajando en ella.
+Stage 1: Proposal. En esta etapa, la idea o mejora propuesta es evaluada y discutida por el comité para determinar
+si es viable y si se continuará trabajando en ella.
 Stage 2: Draft. En esta etapa, se comienza a trabajar en la especificación formal de la propuesta.
-Stage 3: Candidate. En esta etapa, se considera que la propuesta está lo suficientemente madura como para ser implementada en los navegadores y se espera que sea incluida en la próxima versión del estándar.
+Stage 3: Candidate. En esta etapa, se considera que la propuesta está lo suficientemente madura como para ser
+implementada en los navegadores y se espera que sea incluida en la próxima versión del estándar.
 Stage 4: Finished. En esta etapa, la propuesta ha sido aceptada y se incluye en la última versión del estándar.
-Además de las etapas mencionadas, TC39 también se encarga de mantener la compatibilidad entre versiones, de manera que las nuevas características no rompan el código existente.
+Además de las etapas mencionadas, TC39 también se encarga de mantener la compatibilidad entre versiones, de manera
+que las nuevas características no rompan el código existente.
 
-TC39 es el encargado de asegurar que javascript siga siendo un lenguaje de programación moderno y relevante. Los miembros de TC39 son expertos en javascript y sus decisiones afectan directamente a la forma en que se desarrolla el lenguaje y a las características que estarán disponibles en el futuro.
+TC39 es el encargado de asegurar que javascript siga siendo un lenguaje de programación moderno y relevante.
+Los miembros de TC39 son expertos en javascript y sus decisiones afectan directamente a la forma en que se desarrolla
+el lenguaje y a las características que estarán disponibles en el futuro.
 */
